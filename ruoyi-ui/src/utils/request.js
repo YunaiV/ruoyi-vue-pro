3436f77c4a47c05474d93d9e9a643b97c7816161
RuoYi <yzz_ivy@163.com>
2019-12-31 17:18:56 +0800

    request.js缺少Message引用

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Notification, MessageBox } from 'element-ui'
+import { Notification, MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
