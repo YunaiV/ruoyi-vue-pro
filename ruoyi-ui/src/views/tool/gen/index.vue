8448b2b9933f271044cc5d5e06bb7ae68a1f22e3
RuoYi <yzz_ivy@163.com>
2020-04-21 18:31:11 +0800

    代码生成序号修正

--- ruoyi-ui/src/views/tool/gen/index.vue
+++ ruoyi-ui/src/views/tool/gen/index.vue
@@ -80,7 +80,11 @@
 
     <el-table v-loading="loading" :data="tableList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column label="序号" align="center" prop="tableId" width="60px" />
+      <el-table-column label="序号" type="index" width="50" align="center">
+        <template slot-scope="scope">
+          <span>{{(queryParams.pageNum - 1) * queryParams.pageSize + scope.$index + 1}}</span>
+        </template>
+      </el-table-column>
       <el-table-column
         label="表名称"
         align="center"
