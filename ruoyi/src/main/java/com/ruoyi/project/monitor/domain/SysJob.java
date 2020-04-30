066d3f3f9f16dcf71073db1a07a4ba7dcfccc2f0
RuoYi <yzz_ivy@163.com>
2020-04-30 12:11:53 +0800

    修改定时任务详情下次执行时间日期显示错误

--- ruoyi/src/main/java/com/ruoyi/project/monitor/domain/SysJob.java
+++ ruoyi/src/main/java/com/ruoyi/project/monitor/domain/SysJob.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.common.constant.ScheduleConstants;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.common.utils.job.CronUtils;
@@ -110,6 +111,7 @@ public class SysJob extends BaseEntity implements Serializable
         this.cronExpression = cronExpression;
     }
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     public Date getNextValidTime()
     {
         if (StringUtils.isNotEmpty(cronExpression))
