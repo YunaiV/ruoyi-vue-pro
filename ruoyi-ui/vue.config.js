0c069a57ccb294b7fbffd6e9d43c7a7f93394da3
Live <1005297262@qq.com>
2020-06-09 11:37:39 +0800

    项目添加robots.txt 防止后台管理系统被搜索引擎收录

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -117,7 +117,11 @@ module.exports = {
                 }
               }
             })
-          config.optimization.runtimeChunk('single')
+          config.optimization.runtimeChunk('single'),
+          {
+             from: path.resolve(__dirname, './public/robots.txt'),//防爬虫文件
+             to:'./',//到根目录下
+          }
         }
       )
   }
