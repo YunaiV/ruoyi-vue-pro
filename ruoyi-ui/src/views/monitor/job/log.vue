a195df175f3031d682eccd1fc764ad2033d92b5f
RuoYi <yzz_ivy@163.com>
2020-06-18 15:07:56 +0800

    修正定时任务日志权限字符

--- ruoyi-ui/src/views/monitor/job/log.vue
+++ ruoyi-ui/src/views/monitor/job/log.vue
@@ -87,7 +87,7 @@
           icon="el-icon-download"
           size="mini"
           @click="handleExport"
-          v-hasPermi="['monitor:jobLog:export']"
+          v-hasPermi="['monitor:job:export']"
         >导出</el-button>
       </el-col>
     </el-row>
