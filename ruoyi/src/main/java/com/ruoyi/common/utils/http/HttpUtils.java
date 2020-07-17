aad58640dcaab7bed4f9720b57bb7f0cc2c4e343
Sxile <3440626@qq.com>
2020-07-17 11:10:14 +0800

    翻转""为equals调用方，防止空指针异常

--- ruoyi/src/main/java/com/ruoyi/common/utils/http/HttpUtils.java
+++ ruoyi/src/main/java/com/ruoyi/common/utils/http/HttpUtils.java
@@ -204,7 +204,7 @@ public class HttpUtils
             String ret = "";
             while ((ret = br.readLine()) != null)
             {
-                if (ret != null && !ret.trim().equals(""))
+                if (ret != null && !"".equals(ret.trim()))
                 {
                     result.append(new String(ret.getBytes("ISO-8859-1"), "utf-8"));
                 }
