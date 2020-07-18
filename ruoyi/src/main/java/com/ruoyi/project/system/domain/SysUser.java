11ab4b4832f8d9222646d083c92204d7b2b7f5fc
RuoYi <yzz_ivy@163.com>
2020-07-18 12:32:22 +0800

    缺少JsonIgnore包路径。

--- ruoyi/src/main/java/com/ruoyi/project/system/domain/SysUser.java
+++ ruoyi/src/main/java/com/ruoyi/project/system/domain/SysUser.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ruoyi.framework.aspectj.lang.annotation.Excel;
 import com.ruoyi.framework.aspectj.lang.annotation.Excel.ColumnType;
@@ -196,6 +197,7 @@ public class SysUser extends BaseEntity
     {
         this.avatar = avatar;
     }
+    
 	@JsonIgnore
     @JsonProperty
     public String getPassword()
