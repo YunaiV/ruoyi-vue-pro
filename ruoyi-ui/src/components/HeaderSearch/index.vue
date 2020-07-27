fe030cc02248f2a0481d09182e2e9c94b9bbd77f
RuoYi <yzz_ivy@163.com>
2020-07-27 16:55:51 +0800

    修改 node-sass 为 dart-sass

--- ruoyi-ui/src/components/HeaderSearch/index.vue
+++ ruoyi-ui/src/components/HeaderSearch/index.vue
@@ -167,7 +167,7 @@ export default {
     display: inline-block;
     vertical-align: middle;
 
-    /deep/ .el-input__inner {
+    ::v-deep .el-input__inner {
       border-radius: 0;
       border: 0;
       padding-left: 0;
