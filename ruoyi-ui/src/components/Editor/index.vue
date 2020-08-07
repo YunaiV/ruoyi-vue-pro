d0b315f4a594ab942084efd54aa84110c3870556
RuoYi <yzz_ivy@163.com>
2020-08-07 16:03:45 +0800

    删除重复的placeholder

--- ruoyi-ui/src/components/Editor/index.vue
+++ ruoyi-ui/src/components/Editor/index.vue
@@ -66,7 +66,6 @@ export default {
       content: this.value,
       uploadImgUrl: "",
       editorOption: {
-        placeholder: "",
         theme: "snow", // or 'bubble'
         placeholder: "请输入内容",
         modules: {
