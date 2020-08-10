a78211ca00d0261349cd4e761e4ddb70aadd6469
RuoYi <yzz_ivy@163.com>
2020-08-10 12:20:52 +0800

    优化上级菜单不能选择自己

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysMenuController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysMenuController.java
@@ -128,6 +128,10 @@ public class SysMenuController extends BaseController
         {
             return AjaxResult.error("新增菜单'" + menu.getMenuName() + "'失败，地址必须以http(s)://开头");
         }
+        else if (menu.getMenuId().equals(menu.getParentId()))
+        {
+            return AjaxResult.error("新增菜单'" + menu.getMenuName() + "'失败，上级菜单不能选择自己");
+        }
         menu.setUpdateBy(SecurityUtils.getUsername());
         return toAjax(menuService.updateMenu(menu));
     }
