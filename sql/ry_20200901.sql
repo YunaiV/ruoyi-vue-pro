4f86b6d9ca98b0feb6474c0c775852e7709cadcb
RuoYi <yzz_ivy@163.com>
2020-09-01 18:07:59 +0800

    修改公告内容字段类型

--- sql/ry_20200901.sql
+++ sql/ry_20200901.sql
@@ -603,7 +603,7 @@ create table sys_notice (
   notice_id         int(4)          not null auto_increment    comment '公告ID',
   notice_title      varchar(50)     not null                   comment '公告标题',
   notice_type       char(1)         not null                   comment '公告类型（1通知 2公告）',
-  notice_content    blob            default null               comment '公告内容',
+  notice_content    longblob        default null               comment '公告内容',
   status            char(1)         default '0'                comment '公告状态（0正常 1关闭）',
   create_by         varchar(64)     default ''                 comment '创建者',
   create_time       datetime                                   comment '创建时间',
