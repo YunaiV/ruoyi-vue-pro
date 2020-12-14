8988d0b4ab2430ed13e0672b94e3c45de0112207
RuoYi <yzz_ivy@163.com>
2020-12-14 09:02:10 +0800

    若依 3.3.0

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -146,6 +146,64 @@
             <span>更新日志</span>
           </div>
           <el-collapse accordion>
+            <el-collapse-item title="v3.3.0 - 2020-12-14">
+              <ol>
+                <li>新增缓存监控功能</li>
+                <li>支持主题风格配置</li>
+                <li>修复多级菜单之间切换无法缓存的问题</li>
+                <li>多级菜单自动配置组件</li>
+                <li>代码生成预览支持高亮显示</li>
+                <li>支持Get请求映射Params参数</li>
+                <li>删除用户和角色解绑关联</li>
+                <li>去除用户手机邮箱部门必填验证</li>
+                <li>Excel支持注解align对齐方式</li>
+                <li>Excel支持导入Boolean型数据</li>
+                <li>优化头像样式，鼠标移入悬停遮罩</li>
+                <li>代码生成预览提供滚动机制</li>
+                <li>代码生成删除多余的数字float类型</li>
+                <li>修正转换字符串的目标字符集属性</li>
+                <li>回显数据字典防止空值报错</li>
+                <li>日志记录增加过滤多文件场景</li>
+                <li>修改缓存Set方法可能导致嵌套的问题</li>
+                <li>移除前端一些多余的依赖</li>
+                <li>防止安全扫描YUI出现的风险提示</li>
+                <li>修改node-sass为dart-sass</li>
+                <li>升级SpringBoot到最新版本2.1.18</li>
+                <li>升级poi到最新版本4.1.2</li>
+                <li>升级oshi到最新版本v5.3.6</li>
+                <li>升级bitwalker到最新版本1.21</li>
+                <li>升级axios到最新版本0.21.0</li>
+                <li>升级element-ui到最新版本2.14.1</li>
+                <li>升级vue到最新版本2.6.12</li>
+                <li>升级vuex到最新版本3.6.0</li>
+                <li>升级vue-cli到版本4.5.9</li>
+                <li>升级vue-router到最新版本3.4.9</li>
+                <li>升级vue-cli到最新版本4.4.6</li>
+                <li>升级vue-cropper到最新版本0.5.5</li>
+                <li>升级clipboard到最新版本2.0.6</li>
+                <li>升级core-js到最新版本3.8.1</li>
+                <li>升级echarts到最新版本4.9.0</li>
+                <li>升级file-saver到最新版本2.0.4</li>
+                <li>升级fuse.js到最新版本6.4.3</li>
+                <li>升级js-beautify到最新版本1.13.0</li>
+                <li>升级js-cookie到最新版本2.2.1</li>
+                <li>升级path-to-regexp到最新版本6.2.0</li>
+                <li>升级quill到最新版本1.3.7</li>
+                <li>升级screenfull到最新版本5.0.2</li>
+                <li>升级sortablejs到最新版本1.10.2</li>
+                <li>升级vuedraggable到最新版本2.24.3</li>
+                <li>升级chalk到最新版本4.1.0</li>
+                <li>升级eslint到最新版本7.15.0</li>
+                <li>升级eslint-plugin-vue到最新版本7.2.0</li>
+                <li>升级lint-staged到最新版本10.5.3</li>
+                <li>升级runjs到最新版本4.4.2</li>
+                <li>升级sass-loader到最新版本10.1.0</li>
+                <li>升级script-ext-html-webpack-plugin到最新版本2.1.5</li>
+                <li>升级svg-sprite-loader到最新版本5.1.1</li>
+                <li>升级vue-template-compiler到最新版本2.6.12</li>
+                <li>其他细节优化</li>
+              </ol>
+            </el-collapse-item>
             <el-collapse-item title="v3.2.1 - 2020-11-18">
               <ol>
                 <li>阻止任意文件下载漏洞</li>
@@ -435,7 +493,7 @@ export default {
   data() {
     return {
       // 版本号
-      version: "3.2.1",
+      version: "3.3.0",
     };
   },
   methods: {
