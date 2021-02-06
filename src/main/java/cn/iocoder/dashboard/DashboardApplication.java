b8015e87b386ffd42fcb92cdcd6fe726b83281a2
YunaiV <zhijiantianya@gmail.com>
2021-02-06 11:45:10 +0800

    提供一个 Spring Boot 读取多配置文件的示例

--- src/main/java/cn/iocoder/dashboard/DashboardApplication.java
+++ src/main/java/cn/iocoder/dashboard/DashboardApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
+@EnableAdminServer // TODO 芋艿：需要迁移出去
 public class DashboardApplication {
 
     public static void main(String[] args) {
+        // 设置读取的配置文件
+        System.setProperty("spring.config.name", "application,db");
+
+        // 启动 Spring Boot
         SpringApplication.run(DashboardApplication.class, args);
     }
 
