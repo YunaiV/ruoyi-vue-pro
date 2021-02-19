32be92c6f3ddac8a4599781ca0259e7dcd6e2be9
YunaiV <zhijiantianya@gmail.com>
2021-02-20 00:42:38 +0800

    增加多环境的配置

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: `http://localhost:48080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
