a7a2725a87cea07231cfe7b6239127078553aa05
neilz <neilz.cn@gmail.com>
2021-03-14 21:06:55 +0800

    回退多余代码

--- src/main/java/cn/iocoder/dashboard/modules/infra/controller/job/vo/job/InfJobBaseVO.java
+++ src/main/java/cn/iocoder/dashboard/modules/infra/controller/job/vo/job/InfJobBaseVO.java
@@ -34,7 +34,5 @@ public class InfJobBaseVO {
     @ApiModelProperty(value = "监控超时时间", example = "1000")
     private Integer monitorTimeout;
 
-    public void setCronExpression(String cronExpression) {
-        this.cronExpression = cronExpression;
-    }
+
 }
