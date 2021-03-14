4067087da6f8615ef3ad07ed03c8332be18e1c90
hexiaowu <hexiaowu1993@gmail.com>
2021-03-14 18:56:06 +0800

    移除自定义的Long序列化字符串类型规则 新增LocalDateTime的序列化、反序列化规则

--- src/main/java/cn/iocoder/dashboard/framework/jackson/config/JacksonConfig.java
+++ src/main/java/cn/iocoder/dashboard/framework/jackson/config/JacksonConfig.java
@@ -1,11 +1,15 @@
 package cn.iocoder.dashboard.framework.jackson.config;
 
-import cn.iocoder.dashboard.framework.jackson.ser.LongSerializer;
+import cn.iocoder.dashboard.framework.jackson.deser.LocalDateTimeDeserializer;
+import cn.iocoder.dashboard.framework.jackson.ser.LocalDateTimeSerializer;
 import cn.iocoder.dashboard.util.json.JsonUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
 
 @Configuration
 public class JacksonConfig {
@@ -15,11 +19,15 @@ public class JacksonConfig {
     public JsonUtils jsonUtils(ObjectMapper objectMapper) {
         SimpleModule simpleModule = new SimpleModule();
         /*
-         * 新增Long类型序列化规则，数值超过2^53-1，在JS会出现精度丢失问题，因此Long自动序列化为字符串类型
-          */
-        simpleModule.addSerializer(Long.class,LongSerializer.getInstance())
-                    .addSerializer(Long.TYPE,LongSerializer.getInstance());
-        objectMapper.registerModule(simpleModule);
+         * 1. 新增Long类型序列化规则，数值超过2^53-1，在JS会出现精度丢失问题，因此Long自动序列化为字符串类型
+         * 2. 新增LocalDateTime序列化、反序列化规则
+         */
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance)
+                    .addSerializer(Long.TYPE, ToStringSerializer.instance)
+                    .addSerializer(LocalDateTime.class, LocalDateTimeSerializer.INSTANCE)
+                    .addDeserializer(LocalDateTime.class, LocalDateTimeDeserializer.INSTANCE);
+
+        objectMapper.registerModules(simpleModule);
 
         JsonUtils.init(objectMapper);
         return new JsonUtils();
