47ac6fd6f501e23f24f528111857436715668dba
YunaiV <zhijiantianya@gmail.com>
2021-04-18 00:28:36 +0800

    将阿里云、云片的短信错误码，进行翻译

--- src/main/java/cn/iocoder/dashboard/framework/sms/core/client/impl/aliyun/AliyunSmsCodeMapping.java
+++ src/main/java/cn/iocoder/dashboard/framework/sms/core/client/impl/aliyun/AliyunSmsCodeMapping.java
@@ -9,6 +9,8 @@ import static cn.iocoder.dashboard.framework.sms.core.enums.SmsFrameworkErrorCod
 /**
  * 阿里云的 SmsCodeMapping 实现类
  *
+ * 参见 https://help.aliyun.com/document_detail/101346.htm 文档
+ *
  * @author 芋道源码
  */
 public class AliyunSmsCodeMapping implements SmsCodeMapping {
@@ -17,11 +19,23 @@ public class AliyunSmsCodeMapping implements SmsCodeMapping {
     public ErrorCode apply(String apiCode) {
         switch (apiCode) {
             case "OK": return GlobalErrorCodeConstants.SUCCESS;
-            case "MissingAccessKeyId": return SMS_CHANNEL_API_KEY_MISSING;
-            case "isp.RAM_PERMISSION_DENY": return SMS_CHANNEL_PERMISSION_DENY;
+            case "isv.ACCOUNT_NOT_EXISTS":
+            case "isv.ACCOUNT_ABNORMAL":
+            case "MissingAccessKeyId": return SMS_ACCOUNT_INVALID;
+            case "isp.RAM_PERMISSION_DENY": return SMS_PERMISSION_DENY;
+            case "isv.INVALID_JSON_PARAM":
             case "isv.INVALID_PARAMETERS": return SMS_API_PARAM_ERROR;
-            case "isv.BUSINESS_LIMIT_CONTROL": return SMS_SEND_LIMIT_CONTROL;
-            case "isv.SMS_TEMPLATE_ILLEGAL": return SMS_TEMPLATE_NOT_EXISTS;
+            case "isv.BUSINESS_LIMIT_CONTROL": return SMS_SEND_BUSINESS_LIMIT_CONTROL;
+            case "isv.DAY_LIMIT_CONTROL": return SMS_SEND_DAY_LIMIT_CONTROL;
+            case "isv.SMS_CONTENT_ILLEGAL": return SMS_SEND_CONTENT_INVALID;
+            case "isv.SMS_TEMPLATE_ILLEGAL": return SMS_TEMPLATE_INVALID;
+            case "isv.SMS_SIGNATURE_ILLEGAL":
+            case "isv.SIGN_NAME_ILLEGAL":
+            case "isv.SMS_SIGN_ILLEGAL": return SMS_SIGN_INVALID;
+            case "isv.AMOUNT_NOT_ENOUGH":
+            case "isv.OUT_OF_SERVICE": return SMS_ACCOUNT_MONEY_NOT_ENOUGH;
+            case "isv.MOBILE_NUMBER_ILLEGAL": return SMS_MOBILE_INVALID;
+            case "isv.TEMPLATE_MISSING_PARAMETERS": return SMS_TEMPLATE_PARAM_ERROR;
         }
         return SMS_UNKNOWN;
     }
