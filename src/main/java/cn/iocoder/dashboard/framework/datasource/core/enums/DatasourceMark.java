ef7089c1b9cf196b2da85e8bdca9a6c67ff11300
YunaiV <zhijiantianya@gmail.com>
2021-04-18 20:39:31 +0800

    完成 druid ad filter 组件 梳理 druid 的配置

---
+++ src/main/java/cn/iocoder/dashboard/framework/datasource/core/enums/DatasourceMark.java
@@ -0,0 +1,14 @@
+package cn.iocoder.dashboard.framework.datasource.core.enums;
+
+/**
+ * 对应于多数据源中不同数据源配置
+ *
+ * 在方法上使用注解{@code  @DS(DatasourceMark.slave)}可以指定slave数据源，默认是master数据源
+ */
+public interface DatasourceMark {
+
+    String MASTER = "master";
+
+    String SLAVE = "slave";
+
+}
