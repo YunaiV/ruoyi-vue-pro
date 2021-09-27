0439a5505afdde71ec3543d708349bfeba776719
YunaiV <zhijiantianya@gmail.com>
2021-09-27 09:47:37 +0800

    初始化数据字典

--- yudao-user-server/src/main/java/cn/iocoder/yudao/userserver/UserServerApplication.java
+++ yudao-user-server/src/main/java/cn/iocoder/yudao/userserver/UserServerApplication.java
@@ -1,14 +1,9 @@
 package cn.iocoder.yudao.userserver;
 
-import cn.iocoder.yudao.framework.dict.config.YudaoDictAutoConfiguration;
-import cn.iocoder.yudao.framework.security.config.YudaoSecurityAutoConfiguration;
-import cn.iocoder.yudao.framework.security.config.YudaoWebSecurityConfigurerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        YudaoDictAutoConfiguration.class,
-})
+@SpringBootApplication
 public class UserServerApplication {
 
     public static void main(String[] args) {
