60b528e80e5a25ead0efc7b2ececb5b4a001a913
QingChen <iqingchen@aliyun.com>
2021-10-12 10:42:17 +0800

    支付核心

---
+++ yudao-framework/yudao-spring-boot-starter-biz-pay/src/main/java/cn/iocoder/yudao/framework/pay/core/domain/pay/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @description
+ * @author Qingchen
+ * @version 1.0.0
+ * @date 2021-10-12 10:42
+ * @class cn.iocoder.yudao.framework.pay.core.domain.pay.package-info.java
+ */
+package cn.iocoder.yudao.framework.pay.core.domain.pay;
