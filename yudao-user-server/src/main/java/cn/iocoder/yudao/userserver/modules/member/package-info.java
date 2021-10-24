60f5898c9af1467f0192de57fee3323b2827a7de
YunaiV <zhijiantianya@gmail.com>
2021-10-24 23:02:34 +0800

    增加 pay.html 前端，对接微信 JS SDK

--- yudao-user-server/src/main/java/cn/iocoder/yudao/userserver/modules/member/package-info.java
+++ yudao-user-server/src/main/java/cn/iocoder/yudao/userserver/modules/member/package-info.java
@@ -1,7 +1,8 @@
 /**
- * member 包下，我们放会员业务.
- * 例如说：会员中心等等
+ * weixin 包下，我们放微信相关业务.
+ * 例如说：微信公众号、等等
+ * ps：微信支付，还是放在 pay 包下
  *
- * 缩写：mbr
+ * 缩写：wx
  */
 package cn.iocoder.yudao.userserver.modules.member;
--- yudao-user-server/src/main/java/cn/iocoder/yudao/userserver/modules/pay/controller/order/PayOrderController.java
+++ yudao-user-server/src/main/java/cn/iocoder/yudao/userserver/modules/pay/controller/order/PayOrderController.java
@@ -12,10 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
