b99f364d92b9b7f2afcb9fe0378113627bfddf6d
YunaiV <zhijiantianya@gmail.com>
2021-12-15 12:27:45 +0800

    修复前端获取租户方法缺失

--- yudao-admin-ui/src/api/system/tenant.js
+++ yudao-admin-ui/src/api/system/tenant.js
@@ -1,5 +1,16 @@
 import request from '@/utils/request'
 
+// 使用租户名，获得租户编号
+export function getTenantIdByName(name) {
+  return request({
+    url: '/system/tenant/get-id-by-name',
+    method: 'get',
+    params: {
+      name
+    }
+  })
+}
+
 // 创建租户
 export function createTenant(data) {
   return request({
