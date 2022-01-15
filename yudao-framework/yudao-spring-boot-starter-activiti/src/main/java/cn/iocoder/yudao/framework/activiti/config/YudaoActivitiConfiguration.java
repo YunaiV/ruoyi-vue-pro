94c115a142e2a9e7fcc3c33dc8f09ccd9d3585d2
YunaiV <zhijiantianya@gmail.com>
2022-01-16 02:55:25 +0800

    优化 activiti 的配置，简单 Mapper XML 的配置，且减少 Mapper 的 SQL 日志的输出~

--- yudao-framework/yudao-spring-boot-starter-activiti/src/main/java/cn/iocoder/yudao/framework/activiti/config/YudaoActivitiConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-activiti/src/main/java/cn/iocoder/yudao/framework/activiti/config/YudaoActivitiConfiguration.java
@@ -4,8 +4,6 @@ import cn.iocoder.yudao.framework.activiti.core.web.ActivitiWebFilter;
 import cn.iocoder.yudao.framework.common.enums.WebFilterOrderEnum;
 import org.activiti.image.ProcessDiagramGenerator;
 import org.activiti.image.impl.DefaultProcessDiagramGenerator;
-import org.activiti.spring.boot.ProcessEngineConfigurationConfigurer;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,14 +17,6 @@ public class YudaoActivitiConfiguration {
     @Bean
     public ProcessDiagramGenerator processDiagramGenerator() {
         return new DefaultProcessDiagramGenerator();
-    }
-
-    /**
-     * ProcessEngineConfigurationConfigurer 实现类，设置使用 MyBatis SqlSessionFactory
-     */
-    @Bean
-    public ProcessEngineConfigurationConfigurer processEngineConfigurationConfigurer(SqlSessionFactory sqlSessionFactory) {
-        return springProcessEngineConfiguration -> springProcessEngineConfiguration.setSqlSessionFactory(sqlSessionFactory);
     }
 
     @Bean
