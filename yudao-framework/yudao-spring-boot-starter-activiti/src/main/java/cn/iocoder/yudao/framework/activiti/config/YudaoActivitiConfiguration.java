ec876d54a4ea85fce87cd1d5b7472e9129df2b17
YunaiV <zhijiantianya@gmail.com>
2022-01-23 12:52:33 +0800

    修复 activiti 事务的一致性

--- yudao-framework/yudao-spring-boot-starter-activiti/src/main/java/cn/iocoder/yudao/framework/activiti/config/YudaoActivitiConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-activiti/src/main/java/cn/iocoder/yudao/framework/activiti/config/YudaoActivitiConfiguration.java
@@ -4,9 +4,15 @@ import cn.iocoder.yudao.framework.activiti.core.web.ActivitiWebFilter;
 import cn.iocoder.yudao.framework.common.enums.WebFilterOrderEnum;
 import org.activiti.image.ProcessDiagramGenerator;
 import org.activiti.image.impl.DefaultProcessDiagramGenerator;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.activiti.spring.boot.ProcessEngineConfigurationConfigurer;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class YudaoActivitiConfiguration {
@@ -25,6 +31,15 @@ public class YudaoActivitiConfiguration {
         registrationBean.setFilter(new ActivitiWebFilter());
         registrationBean.setOrder(WebFilterOrderEnum.ACTIVITI_FILTER);
         return registrationBean;
+    }
+
+    /**
+     * ProcessEngineConfigurationConfigurer 实现类，设置事务管理器，保证 ACT_ 表和自己的表的事务一致性
+     */
+    @Bean
+    public ProcessEngineConfigurationConfigurer processEngineConfigurationConfigurer(
+            PlatformTransactionManager platformTransactionManager) {
+        return processEngineConfiguration -> processEngineConfiguration.setTransactionManager(platformTransactionManager);
     }
 
 }
