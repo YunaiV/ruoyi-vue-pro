7df42db2e25c25f60ef00995c8342c7d702200b4
YunaiV <zhijiantianya@gmail.com>
2022-02-15 21:53:12 +0800

    v3.5.0 富文本工具栏配置视频

--- yudao-ui-admin/src/components/Editor/index.vue
+++ yudao-ui-admin/src/components/Editor/index.vue
@@ -75,7 +75,7 @@ export default {
             [{ color: [] }, { background: [] }],             // 字体颜色、字体背景颜色
             [{ align: [] }],                                 // 对齐方式
             ["clean"],                                       // 清除文本格式
-            ["link", "image"]                                // 链接、图片
+            ["link", "image", "video"]                       // 链接、图片、视频
           ],
         },
         placeholder: "请输入内容",
