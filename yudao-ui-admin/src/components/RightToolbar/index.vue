20e34e35a3bdc48e6a4c58e3849aa729bd18efe5
YunaiV <zhijiantianya@gmail.com>
2022-02-15 19:28:04 +0800

    v3.4.0 表格右侧工具栏组件支持显隐列

--- yudao-ui-admin/src/components/RightToolbar/index.vue
+++ yudao-ui-admin/src/components/RightToolbar/index.vue
@@ -1,4 +1,3 @@
-<!-- @author Shiyn/   huangmx 20200807优化-->
 <template>
   <div class="top-right-btn">
     <el-row>
@@ -8,31 +7,74 @@
       <el-tooltip class="item" effect="dark" content="刷新" placement="top">
         <el-button size="mini" circle icon="el-icon-refresh" @click="refresh()" />
       </el-tooltip>
+      <el-tooltip class="item" effect="dark" content="显隐列" placement="top" v-if="columns">
+        <el-button size="mini" circle icon="el-icon-menu" @click="showColumn()" />
+      </el-tooltip>
     </el-row>
+    <el-dialog :title="title" :visible.sync="open" append-to-body>
+      <el-transfer
+        :titles="['显示', '隐藏']"
+        v-model="value"
+        :data="columns"
+        @change="dataChange"
+      ></el-transfer>
+    </el-dialog>
   </div>
 </template>
 <script>
 export default {
   name: "RightToolbar",
   data() {
-    return {};
+    return {
+      // 显隐数据
+      value: [],
+      // 弹出层标题
+      title: "显示/隐藏",
+      // 是否显示弹出层
+      open: false,
+    };
   },
   props: {
     showSearch: {
       type: Boolean,
       default: true,
     },
+    columns: {
+      type: Array,
+    },
   },
 
   methods: {
-    //搜索
+    // 搜索
     toggleSearch() {
       this.$emit("update:showSearch", !this.showSearch);
     },
-    //刷新
+    // 刷新
     refresh() {
       this.$emit("queryTable");
+    },
+    // 右侧列表元素变化
+    dataChange(data) {
+      for (var item in this.columns) {
+        const key = this.columns[item].key;
+        this.columns[item].visible = !data.includes(key);
+      }
+    },
+    // 打开显隐列dialog
+    showColumn() {
+      this.open = true;
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+::v-deep .el-transfer__button {
+  border-radius: 50%;
+  padding: 12px;
+  display: block;
+  margin-left: 0px;
+}
+::v-deep .el-transfer__button:first-child {
+  margin-bottom: 10px;
+}
+</style>
--- yudao-ui-admin/src/views/system/user/index.vue
+++ yudao-ui-admin/src/views/system/user/index.vue
@@ -50,21 +50,21 @@
             <el-button type="warning" icon="el-icon-download" size="mini" @click="handleExport"
                        v-hasPermi="['system:user:export']">导出</el-button>
           </el-col>
-          <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+          <right-toolbar :showSearch.sync="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
         </el-row>
 
         <el-table v-loading="loading" :data="userList">
-          <el-table-column label="用户编号" align="center" prop="id" />
-          <el-table-column label="用户名称" align="center" prop="username" :show-overflow-tooltip="true" />
-          <el-table-column label="用户昵称" align="center" prop="nickname" :show-overflow-tooltip="true" />
-          <el-table-column label="部门" align="center" prop="dept.name" :show-overflow-tooltip="true" />
-          <el-table-column label="手机号码" align="center" prop="mobile" width="120" />
-          <el-table-column label="状态" align="center">
+          <el-table-column label="用户编号" align="center" prop="id" v-if="columns[0].visible" />
+          <el-table-column label="用户名称" align="center" prop="username" v-if="columns[1].visible" :show-overflow-tooltip="true" />
+          <el-table-column label="用户昵称" align="center" prop="nickname" v-if="columns[2].visible" :show-overflow-tooltip="true" />
+          <el-table-column label="部门" align="center" prop="dept.name" v-if="columns[3].visible" :show-overflow-tooltip="true" />
+          <el-table-column label="手机号码" align="center" prop="mobile" v-if="columns[4].visible" width="120" />
+          <el-table-column label="状态" v-if="columns[5].visible" align="center">
             <template slot-scope="scope">
               <el-switch v-model="scope.row.status" :active-value="0" :inactive-value="1" @change="handleStatusChange(scope.row)" />
             </template>
           </el-table-column>
-          <el-table-column label="创建时间" align="center" prop="createTime" width="160">
+          <el-table-column label="创建时间" align="center" prop="createTime" v-if="columns[6].visible" width="160">
             <template slot-scope="scope">
               <span>{{ parseTime(scope.row.createTime) }}</span>
             </template>
@@ -317,6 +317,16 @@ export default {
         status: undefined,
         deptId: undefined
       },
+      // 列信息
+      columns: [
+        { key: 0, label: `用户编号`, visible: true },
+        { key: 1, label: `用户名称`, visible: true },
+        { key: 2, label: `用户昵称`, visible: true },
+        { key: 3, label: `部门`, visible: true },
+        { key: 4, label: `手机号码`, visible: true },
+        { key: 5, label: `状态`, visible: true },
+        { key: 6, label: `创建时间`, visible: true }
+      ],
       // 表单校验
       rules: {
         username: [
