7e31efcfe2c62a4aa030f9a4ed2eefa07fa33751
YunaiV <zhijiantianya@gmail.com>
2022-02-17 01:30:08 +0800

    v3.7.0 防止错误token导致的解析异常

--- yudao-ui-admin/src/utils/request.js
+++ yudao-ui-admin/src/utils/request.js
@@ -73,7 +73,7 @@ service.interceptors.response.use(res => {
           location.href = '/index';
         })
       }).catch(() => {});
-      return Promise.reject('error')
+      return Promise.reject()
     } else if (code === 500) {
       Message({
         message: msg,
