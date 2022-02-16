2ce22871461710f4d570081169ca3b2a4620c250
YunaiV <zhijiantianya@gmail.com>
2022-02-17 01:36:32 +0800

    v3.7.0 更新到【默认首页使用keep-alive缓存】后报错

--- yudao-ui-admin/src/components/Breadcrumb/index.vue
+++ yudao-ui-admin/src/components/Breadcrumb/index.vue
@@ -45,7 +45,7 @@ export default {
       if (!name) {
         return false
       }
-      return name.trim() === '首页'
+      return name.trim() === 'Index'
     },
     handleLink(item) {
       const { redirect, path } = item
