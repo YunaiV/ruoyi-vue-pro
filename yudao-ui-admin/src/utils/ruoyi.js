5856c93035143dd42cb93abb4753a98061c54d49
YunaiV <zhijiantianya@gmail.com>
2022-02-17 09:21:32 +0800

    v3.7.0 修复带utc日期格式 yyyy-MM-dd'T'HH:mm:ss.SSS 在safari浏览器中无法正确格式化的问题

--- yudao-ui-admin/src/utils/ruoyi.js
+++ yudao-ui-admin/src/utils/ruoyi.js
@@ -18,7 +18,7 @@ export function parseTime(time, pattern) {
     if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
       time = parseInt(time)
     } else if (typeof time === 'string') {
-      time = time.replace(new RegExp(/-/gm), '/');
+      time = time.replace(new RegExp(/-/gm), '/').replace('T', ' ').replace(new RegExp(/\.[\d]{3}/gm),'');
     }
     if ((typeof time === 'number') && (time.toString().length === 10)) {
       time = time * 1000
