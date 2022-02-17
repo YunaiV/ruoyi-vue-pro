cfd8cd57cfc2a6629e168d646d1b9cdbbb998ac1
YunaiV <zhijiantianya@gmail.com>
2022-02-17 14:33:52 +0800

    v3.8.0 新增认证对象简化权限验证

--- yudao-ui-admin/src/plugins/index.js
+++ yudao-ui-admin/src/plugins/index.js
@@ -1,9 +1,12 @@
+import auth from './auth'
 import cache from './cache'
 import modal from './modal'
 import download from './download'
 
 export default {
   install(Vue) {
+    // 认证对象
+    Vue.prototype.$auth = auth
     // 缓存对象
     Vue.prototype.$cache = cache
     // 模态框对象
