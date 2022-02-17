f749b9db06e5768b209b83a3088507df66df36c3
YunaiV <zhijiantianya@gmail.com>
2022-02-17 09:20:22 +0800

    v3.7.0 自定义可拖动弹窗高度指令

--- yudao-ui-admin/src/directive/dialog/dragHeight.js
+++ yudao-ui-admin/src/directive/dialog/dragHeight.js
@@ -1,5 +1,5 @@
 /**
- * v-dialogDragWidth 可拖动弹窗宽度（右侧边）
+ * v-dialogDragWidth 可拖动弹窗高度（右下角）
  * Copyright (c) 2019 ruoyi
  */
 
@@ -7,20 +7,24 @@ export default {
   bind(el) {
     const dragDom = el.querySelector('.el-dialog');
     const lineEl = document.createElement('div');
-    lineEl.style = 'width: 5px; background: inherit; height: 80%; position: absolute; right: 0; top: 0; bottom: 0; margin: auto; z-index: 1; cursor: w-resize;';
+    lineEl.style = 'width: 6px; background: inherit; height: 10px; position: absolute; right: 0; bottom: 0; margin: auto; z-index: 1; cursor: nwse-resize;';
     lineEl.addEventListener('mousedown',
-      function (e) {
+      function(e) {
         // 鼠标按下，计算当前元素距离可视区的距离
         const disX = e.clientX - el.offsetLeft;
-        // 当前宽度
+        const disY = e.clientY - el.offsetTop;
+        // 当前宽度 高度
         const curWidth = dragDom.offsetWidth;
-        document.onmousemove = function (e) {
+        const curHeight = dragDom.offsetHeight;
+        document.onmousemove = function(e) {
           e.preventDefault(); // 移动时禁用默认事件
           // 通过事件委托，计算移动的距离
-          const l = e.clientX - disX;
-          dragDom.style.width = `${curWidth + l}px`;
+          const xl = e.clientX - disX;
+          const yl = e.clientY - disY
+          dragDom.style.width = `${curWidth + xl}px`;
+          dragDom.style.height = `${curHeight + yl}px`;
         };
-        document.onmouseup = function (e) {
+        document.onmouseup = function(e) {
           document.onmousemove = null;
           document.onmouseup = null;
         };
--- yudao-ui-admin/src/directive/index.js
+++ yudao-ui-admin/src/directive/index.js
@@ -2,12 +2,14 @@ import hasRole from './permission/hasRole'
 import hasPermi from './permission/hasPermi'
 import dialogDrag from './dialog/drag'
 import dialogDragWidth from './dialog/dragWidth'
+import dialogDragHeight from './dialog/dragHeight'
 
 const install = function(Vue) {
   Vue.directive('hasRole', hasRole)
   Vue.directive('hasPermi', hasPermi)
   Vue.directive('dialogDrag', dialogDrag)
   Vue.directive('dialogDragWidth', dialogDragWidth)
+  Vue.directive('dialogDragHeight', dialogDragHeight)
 }
 
 if (window.Vue) {
