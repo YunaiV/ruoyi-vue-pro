ca4290204c462c763f962060b36ce5c3eccb355e
YunaiV <zhijiantianya@gmail.com>
2022-02-17 15:32:36 +0800

    v3.8.0 自定义文字复制剪贴指令

--- yudao-ui-admin/src/directive/index.js
+++ yudao-ui-admin/src/directive/index.js
@@ -3,10 +3,12 @@ import hasPermi from './permission/hasPermi'
 import dialogDrag from './dialog/drag'
 import dialogDragWidth from './dialog/dragWidth'
 import dialogDragHeight from './dialog/dragHeight'
+import clipboard from './module/clipboard'
 
 const install = function(Vue) {
   Vue.directive('hasRole', hasRole)
   Vue.directive('hasPermi', hasPermi)
+  Vue.directive('clipboard', clipboard)
   Vue.directive('dialogDrag', dialogDrag)
   Vue.directive('dialogDragWidth', dialogDragWidth)
   Vue.directive('dialogDragHeight', dialogDragHeight)
