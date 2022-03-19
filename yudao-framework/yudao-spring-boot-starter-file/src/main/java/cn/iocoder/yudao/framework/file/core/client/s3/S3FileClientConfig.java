3583109f90f02e894c2b279ece28a6c1f2bdca4f
YunaiV <zhijiantianya@gmail.com>
2022-03-19 18:05:08 +0800

    优化文件配置，去掉 region 的配置，通过自动识别

--- yudao-framework/yudao-spring-boot-starter-file/src/main/java/cn/iocoder/yudao/framework/file/core/client/s3/S3FileClientConfig.java
+++ yudao-framework/yudao-spring-boot-starter-file/src/main/java/cn/iocoder/yudao/framework/file/core/client/s3/S3FileClientConfig.java
@@ -22,7 +22,7 @@ public class S3FileClientConfig implements FileClientConfig {
 
     /**
      * 节点地址
-     * 1. MinIO：
+     * 1. MinIO：https://www.iocoder.cn/Spring-Boot/MinIO 。例如说，http://127.0.0.1:9000
      * 2. 阿里云：https://help.aliyun.com/document_detail/31837.html
      * 3. 腾讯云：https://cloud.tencent.com/document/product/436/6224
      * 4. 七牛云：https://developer.qiniu.com/kodo/4088/s3-access-domainname
@@ -32,7 +32,7 @@ public class S3FileClientConfig implements FileClientConfig {
     private String endpoint;
     /**
      * 自定义域名
-     * 1. MinIO：
+     * 1. MinIO：通过 Nginx 配置
      * 2. 阿里云：https://help.aliyun.com/document_detail/31836.html
      * 3. 腾讯云：https://cloud.tencent.com/document/product/436/11142
      * 4. 七牛云：https://developer.qiniu.com/kodo/8556/set-the-custom-source-domain-name
@@ -48,7 +48,7 @@ public class S3FileClientConfig implements FileClientConfig {
 
     /**
      * 访问 Key
-     * 1. MinIO：
+     * 1. MinIO：https://www.iocoder.cn/Spring-Boot/MinIO
      * 2. 阿里云：https://ram.console.aliyun.com/manage/ak
      * 3. 腾讯云：https://console.cloud.tencent.com/cam/capi
      * 4. 七牛云：https://portal.qiniu.com/user/key
