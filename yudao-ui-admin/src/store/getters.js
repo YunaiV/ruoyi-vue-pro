98f27227818219b2bd05a3156a4dfe0941b8179c
supine-win <supine.win@gmail.com>
2022-06-11 06:01:53 +0800

    1.beautify user avatar image and show nickname

--- yudao-ui-admin/src/store/getters.js
+++ yudao-ui-admin/src/store/getters.js
@@ -8,6 +8,7 @@ const getters = {
   token: state => state.user.token,
   avatar: state => state.user.avatar,
   name: state => state.user.name,
+  nickname: state => state.user.nickname,
   introduction: state => state.user.introduction,
   roles: state => state.user.roles,
   permissions: state => state.user.permissions,
