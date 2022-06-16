667c8459c0b6048869bdbeff07bccab12aed7bc4
fengdan <fengdan80@outlook.com>
2022-06-16 12:41:27 +0800

    Revert "code review：mp 模块的代码"
    This reverts commit 89a5ea193967e75f103beb821c2cd90a8df9b176.

--- yudao-module-wechat/yudao-module-mp-api/src/main/java/cn/iocoder/yudao/module/mp/enums/ErrorCodeConstants.java
+++ yudao-module-wechat/yudao-module-mp-api/src/main/java/cn/iocoder/yudao/module/mp/enums/ErrorCodeConstants.java
@@ -2,18 +2,16 @@ package cn.iocoder.yudao.module.mp.enums;
 
 import cn.iocoder.yudao.framework.common.exception.ErrorCode;
 
-// TODO @亚洲：错误码要避免和别人的模块分段重叠
 /**
- * MP 错误码枚举类
+ * Member 错误码枚举类
  *
- * mp 系统，使用 1-004-000-000 段
+ * wechatMp 系统，使用 1-004-000-000 段
  */
 public interface ErrorCodeConstants {
 
     // ========== 用户相关  1004001000============
     ErrorCode WX_ACCOUNT_NOT_EXISTS = new ErrorCode(1004001000, "用户不存在");
     ErrorCode WX_ACCOUNT_FANS_NOT_EXISTS = new ErrorCode(1004001000, "用户不存在");
-    // TODO @亚洲：需要前缀哈，错误码分段不要重复
     ErrorCode COMMON_NOT_EXISTS = new ErrorCode(1004001000, "用户不存在");
 
 }
