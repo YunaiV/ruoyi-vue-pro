69a3a835a5a208450bcb172f6a530342aa5f7d7f
YunaiV <zhijiantianya@gmail.com>
2022-06-18 09:22:26 +0800

    新增 `yudao-spring-boot-starter-banner` 组件，用于项目启动时打印开发文档、接口文档等

--- yudao-framework/yudao-spring-boot-starter-banner/src/main/java/cn/iocoder/yudao/framework/banner/core/BannerApplicationRunner.java
+++ yudao-framework/yudao-spring-boot-starter-banner/src/main/java/cn/iocoder/yudao/framework/banner/core/BannerApplicationRunner.java
@@ -1,19 +1,19 @@
-package cn.iocoder.yudao.server.framework.tip.core;
+package cn.iocoder.yudao.framework.banner.core;
 
 import cn.hutool.core.thread.ThreadUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
 /**
  * 项目启动成功后，提供文档相关的地址
+ *
+ * @author 芋道源码
  */
-@Component
 @Slf4j
-public class TipApplicationRunner implements ApplicationRunner {
+public class BannerApplicationRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
