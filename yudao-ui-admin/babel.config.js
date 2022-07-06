083358b89a028ef819d13782edba23a749a02de3
YunaiV <zhijiantianya@gmail.com>
2022-07-06 09:25:06 +0800

    v3.8.3 优化代码

--- yudao-ui-admin/babel.config.js
+++ yudao-ui-admin/babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   'env': {
-    'development': {
+    'production': {
       // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
       // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
       'plugins': ['dynamic-import-node']
