d9c2da7c919508c52fd2826b90758d0372bacc54
YunaiV <zhijiantianya@gmail.com>
2022-07-06 21:45:25 +0800

    1. 【新增】接口支持通过 `@PermitAll` 注解，允许匿名（未登录）进行访问 2. 【新增】`yudao.security.permit-all-urls` 配置项，允许匿名（未登录）进行访问

--- yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/controller/admin/file/FileController.java
+++ yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/controller/admin/file/FileController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -59,6 +60,7 @@ public class FileController {
     }
 
     @GetMapping("/{configId}/get/{path}")
+    @PermitAll
     @ApiOperation("下载文件")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "configId", value = "配置编号",  required = true, dataTypeClass = Long.class),
