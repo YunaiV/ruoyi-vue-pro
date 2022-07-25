54efb3428917413945eba05bbb397b88d47508d8
xingyu <xingyu4j@vip.qq.com>
2022-07-25 18:46:04 +0800

    feat: add vue3 codegen(preview)

--- yudao-ui-admin/src/views/infra/codegen/index.vue
+++ yudao-ui-admin/src/views/infra/codegen/index.vue
@@ -98,7 +98,7 @@ hljs.registerLanguage("html", require("highlight.js/lib/languages/xml"));
 hljs.registerLanguage("vue", require("highlight.js/lib/languages/xml"));
 hljs.registerLanguage("javascript", require("highlight.js/lib/languages/javascript"));
 hljs.registerLanguage("sql", require("highlight.js/lib/languages/sql"));
-
+hljs.registerLanguage("typescript", require("highlight.js/lib/languages/typescript"));
 export default {
   name: "Codegen",
   components: { importTable },
