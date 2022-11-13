38d6b51fcce4b62b0528e9595a9ec2a3e9eb6f3f
YunaiV <zhijiantianya@gmail.com>
2022-11-13 19:53:59 +0800

    v3.8.4:优化页面内嵌iframe切换tab不刷新数据

---
+++ yudao-ui-admin/src/layout/components/IframeToggle/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition-group name="fade-transform" mode="out-in">
+    <inner-link
+        v-for="(item, index) in iframeViews"
+        :key="item.path"
+        :iframeId="'iframe' + index"
+        v-show="$route.path === item.path"
+        :src="item.meta.link"
+    ></inner-link>
+  </transition-group>
+</template>
+
+<script>
+import InnerLink from "../InnerLink/index.vue"
+
+export default {
+  components: { InnerLink },
+  computed: {
+    iframeViews() {
+      return this.$store.state.tagsView.iframeViews
+    }
+  }
+}
+</script>
