ea95115ffd979445a03a47e28e2b60d4633325c5
YunaiV <zhijiantianya@gmail.com>
2022-11-25 20:21:46 +0800

    移除云片短信渠道，解决云片的安全风险

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/admin/sms/SmsCallbackController.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/admin/sms/SmsCallbackController.java
@@ -1,17 +1,14 @@
 package cn.iocoder.yudao.module.system.controller.admin.sms;
 
-import cn.hutool.core.util.URLUtil;
 import cn.hutool.extra.servlet.ServletUtil;
-import cn.iocoder.yudao.module.system.service.sms.SmsSendService;
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.operatelog.core.annotations.OperateLog;
 import cn.iocoder.yudao.framework.sms.core.enums.SmsChannelEnum;
+import cn.iocoder.yudao.module.system.service.sms.SmsSendService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -27,17 +24,6 @@ public class SmsCallbackController {
 
     @Resource
     private SmsSendService smsSendService;
-
-    @PostMapping("/yunpian")
-    @PermitAll
-    @ApiOperation(value = "云片短信的回调", notes = "参见 https://www.yunpian.com/official/document/sms/zh_cn/domestic_push_report 文档")
-    @ApiImplicitParam(name = "sms_status", value = "发送状态", required = true, example = "[{具体内容}]", dataTypeClass = String.class)
-    @OperateLog(enable = false)
-    public String receiveYunpianSmsStatus(@RequestParam("sms_status") String smsStatus) throws Throwable {
-        String text = URLUtil.decode(smsStatus); // decode 解码参数，因为它被 encode
-        smsSendService.receiveSmsStatus(SmsChannelEnum.YUN_PIAN.getCode(), text);
-        return "SUCCESS"; // 约定返回 SUCCESS 为成功
-    }
 
     @PostMapping("/aliyun")
     @PermitAll
