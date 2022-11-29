1583380a27b65a0277000fd7b82e60402494c074
董伟 <happiness0911@126.com>
2022-11-29 16:45:08 +0800

    fix name2
    replace 后台管理系统名称

--- yudao-ui-admin/src/views/login.vue
+++ yudao-ui-admin/src/views/login.vue
@@ -9,7 +9,7 @@
       <div class="field">
         <!-- [移动端]标题 -->
         <h2 class="mobile-title">
-          <h3 class="title">芋道后台管理系统</h3>
+          <h3 class="title">岳阳医院后台管理系统</h3>
         </h2>
 
         <!-- 表单 -->
