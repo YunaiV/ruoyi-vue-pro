0b820657a617e7aef2fd6e5fdd2e0c4e84490718
xingyu4j <xingyu4j@vip.qq.com>
2022-12-01 17:57:55 +0800

    chore: 优化 vite 配置

---
+++ yudao-ui-admin-vue3/build/vite/optimize.ts
@@ -0,0 +1,34 @@
+const include = [
+  'vue',
+  'sass',
+  'vue-router',
+  'vue-types',
+  'vue-i18n',
+  'vxe-table',
+  'vxe-table/es/style',
+  'xe-utils',
+  'lodash-es',
+  'element-plus',
+  'element-plus/es/locale/lang/zh-cn',
+  'element-plus/es/locale/lang/en',
+  '@iconify/iconify',
+  '@vueuse/core',
+  'qs',
+  'axios',
+  'pinia',
+  'dayjs',
+  'qrcode',
+  'echarts/core',
+  'echarts/charts',
+  'echarts/components',
+  'echarts/renderers',
+  'echarts-wordcloud',
+  'intro.js',
+  'crypto-js',
+  '@wangeditor/editor',
+  '@wangeditor/editor-for-vue'
+]
+
+const exclude = ['@iconify/json']
+
+export { include, exclude }
