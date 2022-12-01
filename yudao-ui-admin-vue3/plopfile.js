5b89a30b952e3b07da3bae38e282f2d2b8de5ac0
xingyu4j <xingyu4j@vip.qq.com>
2022-12-01 18:00:43 +0800

    chore: 移除 plop

--- yudao-ui-admin-vue3/plopfile.js
+++
@@ -1,7 +0,0 @@
-const viewGenerator = require('./plop/view/prompt.js')
-const componentGenerator = require('./plop/component/prompt.js')
-
-module.exports = function (plop) {
-  plop.setGenerator('view', viewGenerator)
-  plop.setGenerator('component', componentGenerator)
-}
