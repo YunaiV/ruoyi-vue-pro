88ce5e540e06f3f49383ba0fd93936e55654ad45
xingyu4j <xingyu4j@vip.qq.com>
2022-12-09 14:24:09 +0800

    refactor: springdoc 替换 springfox

--- yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/swagger/config/YudaoSwaggerAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/swagger/config/YudaoSwaggerAutoConfiguration.java
@@ -1,7 +1,6 @@
 package cn.iocoder.yudao.framework.swagger.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -14,6 +13,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+
+import java.util.Arrays;
 
 /**
  * Swagger3 自动配置类
@@ -29,15 +30,30 @@ public class YudaoSwaggerAutoConfiguration {
 
     @Bean
     public OpenAPI createRestApi(SwaggerProperties properties) {
-        return new OpenAPI()
-                .info(new Info().title(properties.getTitle())
-                        .description(properties.getDescription())
-                        .version(properties.getVersion())
-                        .license(new License().name("MIT").url("https://gitee.com/zhijiantianya/ruoyi-vue-pro/blob/master/LICENSE")))
-                .externalDocs(new ExternalDocumentation()
-                        .description(properties.getDescription())
-                        .url("https://gitee.com/zhijiantianya/ruoyi-vue-pro"));
+        //信息
+        Info info = new Info()
+                .title(properties.getTitle())
+                .description(properties.getDescription())
+                .version(properties.getVersion())
+                .license(new License().name("MIT").url("https://gitee.com/zhijiantianya/ruoyi-vue-pro/blob/master/LICENSE"));
+        //鉴权组件(随便起名的)
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.OAUTH2)
+                .scheme("bearer")//固定写法
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+        Components components = new Components()
+                .addSecuritySchemes("bearer", securityScheme);
 
+        //鉴权限制要求(随便起名的)
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearer", Arrays.asList("read", "write"));
+
+        return new OpenAPI()
+                .info(info)
+                .components(components)
+                .addSecurityItem(securityRequirement);
     }
 
     @Bean
@@ -55,6 +71,5 @@ public class YudaoSwaggerAutoConfiguration {
                 .pathsToMatch("/admin/**")
                 .build();
     }
-
 
 }
