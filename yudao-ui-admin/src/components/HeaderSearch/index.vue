d86c36a151a09ec1e0ee65282f9ad8dc338513ce
xingyu <xingyu4j@vip.qq.com>
2022-12-27 11:18:06 +0800

    vue2 圆角标签栏

--- yudao-ui-admin/src/components/HeaderSearch/index.vue
+++ yudao-ui-admin/src/components/HeaderSearch/index.vue
@@ -88,7 +88,6 @@ export default {
         threshold: 0.4,
         location: 0,
         distance: 100,
-        maxPatternLength: 32,
         minMatchCharLength: 1,
         keys: [{
           name: 'title',
