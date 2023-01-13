4941a43e082d9e546aa209e9f53c7db50851c53c
xingyu <xingyu4j@vip.qq.com>
2023-01-13 23:58:14 +0800

    perf: 重构代码生成 优化性能

--- yudao-ui-admin-vue3/src/views/infra/codegen/components/index.ts
+++ yudao-ui-admin-vue3/src/views/infra/codegen/components/index.ts
@@ -1,6 +1,5 @@
 import BasicInfoForm from './BasicInfoForm.vue'
 import CloumInfoForm from './CloumInfoForm.vue'
-import GenInfoForm from './GenInfoForm.vue'
 import ImportTable from './ImportTable.vue'
 import Preview from './Preview.vue'
-export { BasicInfoForm, CloumInfoForm, GenInfoForm, ImportTable, Preview }
+export { BasicInfoForm, CloumInfoForm, ImportTable, Preview }
