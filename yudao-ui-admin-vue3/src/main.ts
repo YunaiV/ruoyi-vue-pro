9ab82888517c674973345ee7349ae076065655bf
xingyuv <xingyu4j@vip.qq.com>
2023-02-06 11:42:35 +0800

    fix: error

--- yudao-ui-admin-vue3/src/main.ts
+++ yudao-ui-admin-vue3/src/main.ts
@@ -52,7 +52,7 @@ import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
 import hljs from 'highlight.js' //导入代码高亮文件
 import 'highlight.js/styles/github.css' //导入代码高亮样式  新版
 
-import Logger from '@/utils/logger'
+import Logger from '@/utils/Logger'
 
 // 本地开发模式 全局引入 element-plus 样式，加快第一次进入速度
 if (isDevMode() == true) {
