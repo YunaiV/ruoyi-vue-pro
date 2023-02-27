2083fb78466938a3f0ca6250cbfc3b0139524e53
gaibu <1016771049@qq.com>
2023-02-27 22:29:40 +0800

    test: 修改 code review

--- yudao-framework/yudao-spring-boot-starter-biz-dict/src/test/java/cn/iocoder/yudao/framework/dict/core/util/DictFrameworkUtilsTest.java
+++ yudao-framework/yudao-spring-boot-starter-biz-dict/src/test/java/cn/iocoder/yudao/framework/dict/core/util/DictFrameworkUtilsTest.java
@@ -2,13 +2,13 @@ package cn.iocoder.yudao.framework.dict.core.util;
 
 import cn.iocoder.yudao.framework.common.enums.CommonStatusEnum;
 import cn.iocoder.yudao.framework.test.core.ut.BaseMockitoUnitTest;
-import cn.iocoder.yudao.framework.test.core.util.RandomUtils;
 import cn.iocoder.yudao.module.system.api.dict.DictDataApi;
 import cn.iocoder.yudao.module.system.api.dict.dto.DictDataRespDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import static cn.iocoder.yudao.framework.test.core.util.RandomUtils.randomPojo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -26,28 +26,23 @@ public class DictFrameworkUtilsTest extends BaseMockitoUnitTest {
     }
 
     @Test
-    public void getDictDataLabelTest() {
-        DictDataRespDTO dataRespDTO = randomPojo();
+    public void testGetDictDataLabel() {
+        // mock 数据
+        DictDataRespDTO dataRespDTO = randomPojo(DictDataRespDTO.class, o -> o.setStatus(CommonStatusEnum.ENABLE.getStatus()));
+        // mock 方法
         when(dictDataApi.getDictData(dataRespDTO.getDictType(), dataRespDTO.getValue())).thenReturn(dataRespDTO);
-        String dictDataLabel = DictFrameworkUtils.getDictDataLabel(dataRespDTO.getDictType(), dataRespDTO.getValue());
-        assertEquals(dataRespDTO.getLabel(), dictDataLabel);
+        // 断言返回值
+        assertEquals(dataRespDTO.getLabel(), DictFrameworkUtils.getDictDataLabel(dataRespDTO.getDictType(), dataRespDTO.getValue()));
     }
 
     @Test
-    public void parseDictDataValueTest() {
-        DictDataRespDTO resp = randomPojo();
+    public void testParseDictDataValue() {
+        // mock 数据
+        DictDataRespDTO resp = randomPojo(DictDataRespDTO.class, o -> o.setStatus(CommonStatusEnum.ENABLE.getStatus()));
+        // mock 方法
         when(dictDataApi.parseDictData(resp.getDictType(), resp.getLabel())).thenReturn(resp);
-        String value = DictFrameworkUtils.parseDictDataValue(resp.getDictType(), resp.getLabel());
-        assertEquals(resp.getValue(), value);
+        // 断言返回值
+        assertEquals(resp.getValue(), DictFrameworkUtils.parseDictDataValue(resp.getDictType(), resp.getLabel()));
     }
 
-    private DictDataRespDTO randomPojo() {
-        DictDataRespDTO dataRespDTO = new DictDataRespDTO();
-        dataRespDTO.setLabel(RandomUtils.randomString());
-        dataRespDTO.setValue(RandomUtils.randomString());
-        dataRespDTO.setDictType(RandomUtils.randomString());
-        dataRespDTO.setStatus(CommonStatusEnum.ENABLE.getStatus());
-
-        return dataRespDTO;
-    }
 }
