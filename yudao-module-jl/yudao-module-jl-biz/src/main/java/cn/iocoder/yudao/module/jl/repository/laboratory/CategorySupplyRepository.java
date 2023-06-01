1ff21878ebb3b69dd03393fbea2e347649922233
chenkaic4 <chenkaic4@gmail.com>
2023-06-02 07:57:21 +0800

    修复JPA删除操作

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/repository/laboratory/CategorySupplyRepository.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/repository/laboratory/CategorySupplyRepository.java
@@ -3,11 +3,16 @@ package cn.iocoder.yudao.module.jl.repository.laboratory;
 import cn.iocoder.yudao.module.jl.entity.laboratory.CategorySupply;
 import org.springframework.data.jpa.repository.*;
 
+import javax.transaction.Transactional;
+
 /**
 * CategorySupplyRepository
 *
 */
 public interface CategorySupplyRepository extends JpaRepository<CategorySupply, Long>, JpaSpecificationExecutor<CategorySupply> {
 
+    @Modifying
+    @Transactional
+    @Query("delete from CategorySupply supply where supply.categoryId = ?1")
     void deleteByCategoryId(Long categoryId);
 }
