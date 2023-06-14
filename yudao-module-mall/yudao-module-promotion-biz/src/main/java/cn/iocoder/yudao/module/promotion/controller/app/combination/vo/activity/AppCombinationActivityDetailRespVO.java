b74991e24261edde587713ffbe9b612c56d5be50
YunaiV <zhijiantianya@gmail.com>
2023-06-14 23:08:22 +0800

    mall + trade：mock 拼团活动详情的接口

--- yudao-module-mall/yudao-module-promotion-biz/src/main/java/cn/iocoder/yudao/module/promotion/controller/app/combination/vo/activity/AppCombinationActivityDetailRespVO.java
+++ yudao-module-mall/yudao-module-promotion-biz/src/main/java/cn/iocoder/yudao/module/promotion/controller/app/combination/vo/activity/AppCombinationActivityDetailRespVO.java
@@ -1,4 +1,4 @@
-package cn.iocoder.yudao.module.promotion.controller.app.seckill.vo;
+package cn.iocoder.yudao.module.promotion.controller.app.combination.vo.activity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -6,20 +6,18 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Schema(description = "用户 App - 秒杀活动 Response VO")
+@Schema(description = "用户 App - 拼团活动明细 Response VO")
 @Data
-public class AppSeckillActivitiDetailRespVO {
+public class AppCombinationActivityDetailRespVO {
 
-    @Schema(description = "秒杀活动编号", required = true, example = "1024")
+    @Schema(description = "拼团活动编号", required = true, example = "1024")
     private Long id;
 
-    @Schema(description = "秒杀活动名称", required = true, example = "晚九点限时秒杀")
+    @Schema(description = "拼团活动名称", required = true, example = "618 大拼团")
     private String name;
 
     @Schema(description = "活动状态", required = true, example = "1")
     private Integer status;
-
-    // TODO @芋艿：开始时间、结束时间，要和场次结合起来；就是要算到当前场次，是几点哈；
 
     @Schema(description = "活动开始时间", required = true)
     private LocalDateTime startTime;
@@ -27,14 +25,17 @@ public class AppSeckillActivitiDetailRespVO {
     @Schema(description = "活动结束时间", required = true)
     private LocalDateTime endTime;
 
-
-    private Long successGruopCount;
-
     @Schema(description = "商品 SPU 编号", required = true, example = "2048")
     private Long spuId;
 
     @Schema(description = "商品信息数组", required = true)
     private List<Product> products;
+
+    @Schema(description = "成功的拼团记录", required = true)
+    private List<Record> successRecords;
+
+    @Schema(description = "进行中的拼团记录", required = true)
+    private List<Record> runningRecords;
 
     @Schema(description = "商品信息")
     @Data
@@ -43,14 +44,23 @@ public class AppSeckillActivitiDetailRespVO {
         @Schema(description = "商品 SKU 编号", required = true, example = "4096")
         private Long skuId;
 
-        @Schema(description = "秒杀金额，单位：分", required = true, example = "100")
-        private Integer seckillPrice;
+        @Schema(description = "拼团金额，单位：分", required = true, example = "100")
+        private Integer combinationPrice;
 
-        @Schema(description = "秒杀限量库存", required = true, example = "50")
+        @Schema(description = "拼团限量库存", required = true, example = "50")
         private Integer quota;
 
         @Schema(description = "每人限购数量", required = true, example = "10")
         private Integer limitCount;
+
+    }
+
+    @Schema(description = "拼团记录")
+    @Data
+    public static class Record {
+
+        @Schema(description = "拼团记录编号", required = true, example = "1024")
+        private Long id;
 
     }
 
