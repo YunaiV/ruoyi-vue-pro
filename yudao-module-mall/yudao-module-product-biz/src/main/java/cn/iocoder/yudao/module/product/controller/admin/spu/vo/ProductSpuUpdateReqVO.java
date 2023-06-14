e23fb5024a386503c704205b92947405a96387f0
xingyu <xingyu4j@vip.qq.com>
2023-06-14 15:21:41 +0800

    fix: required = true 已过期

--- yudao-module-mall/yudao-module-product-biz/src/main/java/cn/iocoder/yudao/module/product/controller/admin/spu/vo/ProductSpuUpdateReqVO.java
+++ yudao-module-mall/yudao-module-product-biz/src/main/java/cn/iocoder/yudao/module/product/controller/admin/spu/vo/ProductSpuUpdateReqVO.java
@@ -27,13 +27,13 @@ public class ProductSpuUpdateReqVO extends ProductSpuBaseVO {
     @NotNull(message = "商品编号不能为空")
     private Long id;
 
-    @Schema(description = "商品销量", required = true, example = "1999")
+    @Schema(description = "商品销量", requiredMode = Schema.RequiredMode.REQUIRED, example = "1999")
     private Integer salesCount;
 
-    @Schema(description = "浏览量", required = true, example = "1999")
+    @Schema(description = "浏览量", requiredMode = Schema.RequiredMode.REQUIRED, example = "1999")
     private Integer browseCount;
 
-    @Schema(description = "商品状态", required = true, example = "1")
+    @Schema(description = "商品状态", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     @InEnum(ProductSpuStatusEnum.class)
     private Integer status;
 
