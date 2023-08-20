281ba1e369ac3810e753ebbb1d925bfcce316e1f
YunaiV <zhijiantianya@gmail.com>
2023-08-20 19:38:17 +0800

    code review：调整错误码分段

--- yudao-module-member/yudao-module-member-api/src/main/java/cn/iocoder/yudao/module/member/enums/ErrorCodeConstants.java
+++ yudao-module-member/yudao-module-member-api/src/main/java/cn/iocoder/yudao/module/member/enums/ErrorCodeConstants.java
@@ -17,7 +17,6 @@ public interface ErrorCodeConstants {
     // ========== AUTH 模块 1004003000 ==========
     ErrorCode AUTH_LOGIN_BAD_CREDENTIALS = new ErrorCode(1004003000, "登录失败，账号密码不正确");
     ErrorCode AUTH_LOGIN_USER_DISABLED = new ErrorCode(1004003001, "登录失败，账号被禁用");
-    ErrorCode AUTH_TOKEN_EXPIRED = new ErrorCode(1004003004, "Token 已经过期");
     ErrorCode AUTH_THIRD_LOGIN_NOT_BIND = new ErrorCode(1004003005, "未绑定账号，需要进行绑定");
     ErrorCode AUTH_WEIXIN_MINI_APP_PHONE_CODE_ERROR = new ErrorCode(1004003006, "获得手机号失败");
     ErrorCode AUTH_MOBILE_USED = new ErrorCode(1004003007, "手机号已经被使用");
@@ -25,20 +24,18 @@ public interface ErrorCodeConstants {
     // ========== 用户收件地址 1004004000 ==========
     ErrorCode ADDRESS_NOT_EXISTS = new ErrorCode(1004004000, "用户收件地址不存在");
 
-    //========== 用户积分 1004005000 ==========
-
-    // TODO @xiaqing：错误码要分段；例如说这里，积分配置、积分记录、签到配置、签到记录；分成 4 段；
-
-    ErrorCode SIGN_IN_CONFIG_NOT_EXISTS = new ErrorCode(1004005003, "签到天数规则不存在");
-    ErrorCode SIGN_IN_CONFIG_EXISTS = new ErrorCode(1004005004, "签到天数规则已存在");
-
-    ErrorCode RECORD_NOT_EXISTS = new ErrorCode( 1004005005, "用户积分记录不存在");
-
-    ErrorCode SIGN_IN_RECORD_NOT_EXISTS = new ErrorCode(1004005006, "签到记录不存在");
-
-
     //========== 会员标签 1004006000 ==========
     ErrorCode TAG_NOT_EXISTS = new ErrorCode(1004006000, "会员标签不存在");
     ErrorCode TAG_NAME_EXISTS = new ErrorCode(1004006001, "会员标签已经存在");
+
+    //========== 积分配置 1004007000 ==========
+
+    //========== 积分记录 1004008000 ==========
+
+    //========== 签到配置 1004009000 ==========
+    ErrorCode SIGN_IN_CONFIG_NOT_EXISTS = new ErrorCode(1004009000, "签到天数规则不存在");
+    ErrorCode SIGN_IN_CONFIG_EXISTS = new ErrorCode(1004009001, "签到天数规则已存在");
+
+    //========== 签到配置 1004010000 ==========
 
 }
