99dd33c4080082244ebfbb50a8ff44aaa4dfa6bc
YunaiV <zhijiantianya@gmail.com>
2023-08-26 20:57:49 +0800

    code review：会员中心的详情返回 totalPoint 积分

--- yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/controller/admin/user/vo/MemberUserRespVO.java
+++ yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/controller/admin/user/vo/MemberUserRespVO.java
@@ -31,8 +31,11 @@ public class MemberUserRespVO extends MemberUserBaseVO {
 
     // ========== 其它信息 ==========
 
-    @Schema(description = "积分", example = "100")
+    @Schema(description = "积分", requiredMode  = Schema.RequiredMode.REQUIRED, example = "100")
     private Integer point;
+
+    @Schema(description = "总积分", requiredMode = Schema.RequiredMode.REQUIRED, example = "2000")
+    private Integer totalPoint;
 
     @Schema(description = "会员标签", example = "[红色, 快乐]")
     private List<String> tagNames;
@@ -43,7 +46,7 @@ public class MemberUserRespVO extends MemberUserBaseVO {
     @Schema(description = "用户分组", example = "购物达人")
     private String groupName;
 
-    @Schema(description = "用户经验值", example = "100")
+    @Schema(description = "用户经验值", requiredMode  = Schema.RequiredMode.REQUIRED, example = "200")
     private Integer experience;
 
 }
