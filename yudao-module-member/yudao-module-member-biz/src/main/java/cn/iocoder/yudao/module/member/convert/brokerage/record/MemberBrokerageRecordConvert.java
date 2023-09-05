c7ef8519c83dc6f0339dd6600ec62ecc37ac4d85
owen <owen@evolsun.com>
2023-09-05 08:56:39 +0800

    member: 分销增加佣金

---
+++ yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/convert/brokerage/record/MemberBrokerageRecordConvert.java
@@ -0,0 +1,46 @@
+package cn.iocoder.yudao.module.member.convert.brokerage.record;
+
+import cn.hutool.core.util.StrUtil;
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.module.member.controller.admin.brokerage.record.vo.MemberBrokerageRecordRespVO;
+import cn.iocoder.yudao.module.member.dal.dataobject.brokerage.record.MemberBrokerageRecordDO;
+import cn.iocoder.yudao.module.member.dal.dataobject.user.MemberUserDO;
+import cn.iocoder.yudao.module.member.enums.brokerage.BrokerageRecordBizTypeEnum;
+import cn.iocoder.yudao.module.member.enums.brokerage.BrokerageRecordStatusEnum;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 佣金记录 Convert
+ *
+ * @author owen
+ */
+@Mapper
+public interface MemberBrokerageRecordConvert {
+
+    MemberBrokerageRecordConvert INSTANCE = Mappers.getMapper(MemberBrokerageRecordConvert.class);
+
+    MemberBrokerageRecordRespVO convert(MemberBrokerageRecordDO bean);
+
+    List<MemberBrokerageRecordRespVO> convertList(List<MemberBrokerageRecordDO> list);
+
+    PageResult<MemberBrokerageRecordRespVO> convertPage(PageResult<MemberBrokerageRecordDO> page);
+
+    default MemberBrokerageRecordDO convert(MemberUserDO user, String bizId, int brokerageFrozenDays, int brokerage, LocalDateTime unfreezeTime) {
+        return new MemberBrokerageRecordDO()
+                .setUserId(user.getId())
+                .setBizType(BrokerageRecordBizTypeEnum.ORDER.getType())
+                .setBizId(bizId)
+                .setPrice(brokerage)
+                .setTotalPrice(user.getBrokeragePrice())
+                .setTitle(BrokerageRecordBizTypeEnum.ORDER.getTitle())
+                .setDescription(StrUtil.format(BrokerageRecordBizTypeEnum.ORDER.getDescription(), String.valueOf(brokerage / 100.0)))
+                .setStatus(BrokerageRecordStatusEnum.WAIT_SETTLEMENT.getStatus())
+                .setFrozenDays(brokerageFrozenDays)
+                .setUnfreezeTime(unfreezeTime);
+    }
+
+}
---
+++ yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/service/brokerage/record/MemberBrokerageRecordService.java
@@ -0,0 +1,40 @@
+package cn.iocoder.yudao.module.member.service.brokerage.record;
+
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.module.member.api.brokerage.dto.BrokerageAddReqDTO;
+import cn.iocoder.yudao.module.member.controller.admin.brokerage.record.vo.MemberBrokerageRecordPageReqVO;
+import cn.iocoder.yudao.module.member.dal.dataobject.brokerage.record.MemberBrokerageRecordDO;
+
+import java.util.List;
+
+/**
+ * 佣金记录 Service 接口
+ *
+ * @author owen
+ */
+public interface MemberBrokerageRecordService {
+
+    /**
+     * 获得佣金记录
+     *
+     * @param id 编号
+     * @return 佣金记录
+     */
+    MemberBrokerageRecordDO getMemberBrokerageRecord(Integer id);
+
+    /**
+     * 获得佣金记录分页
+     *
+     * @param pageReqVO 分页查询
+     * @return 佣金记录分页
+     */
+    PageResult<MemberBrokerageRecordDO> getMemberBrokerageRecordPage(MemberBrokerageRecordPageReqVO pageReqVO);
+
+    /**
+     * 增加佣金
+     *
+     * @param userId 会员ID
+     * @param list   请求参数列表
+     */
+    void addBrokerage(Long userId, List<BrokerageAddReqDTO> list);
+}
