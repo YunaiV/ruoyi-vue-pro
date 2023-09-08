2fd201c7fa944894c7a393075fff568404b5a1f4
lemoncc <4831703@qq.com>
2023-09-08 16:33:49 +0800

    🌈 style: 描述错误, 社交类型的枚举为 SocialTypeEnum 而不是 SysUserSocialTypeEnum

--- yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/controller/app/auth/vo/AppAuthLoginReqVO.java
+++ yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/controller/app/auth/vo/AppAuthLoginReqVO.java
@@ -33,7 +33,7 @@ public class AppAuthLoginReqVO {
 
     // ========== 绑定社交登录时，需要传递如下参数 ==========
 
-    @Schema(description = "社交平台的类型，参见 SysUserSocialTypeEnum 枚举值", requiredMode = Schema.RequiredMode.REQUIRED, example = "10")
+    @Schema(description = "社交平台的类型，参见 SocialTypeEnum 枚举值", requiredMode = Schema.RequiredMode.REQUIRED, example = "10")
     @InEnum(SocialTypeEnum.class)
     private Integer socialType;
 
