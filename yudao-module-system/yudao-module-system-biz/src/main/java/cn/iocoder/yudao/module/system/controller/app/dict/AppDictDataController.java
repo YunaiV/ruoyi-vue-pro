594e4d5fee6a6bc102e7b2a89fb30cc2c7b08b16
owen <owen@evolsun.com>
2023-09-22 15:11:06 +0800

    system: uniapp增加字典数据查询接口

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/app/dict/AppDictDataController.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/app/dict/AppDictDataController.java
@@ -1,4 +1,37 @@
 package cn.iocoder.yudao.module.system.controller.app.dict;
 
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import cn.iocoder.yudao.module.system.controller.admin.dict.vo.data.DictDataExportReqVO;
+import cn.iocoder.yudao.module.system.controller.app.dict.vo.AppDictDataRespVO;
+import cn.iocoder.yudao.module.system.convert.dict.DictDataConvert;
+import cn.iocoder.yudao.module.system.dal.dataobject.dict.DictDataDO;
+import cn.iocoder.yudao.module.system.service.dict.DictDataService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+
+@Tag(name = "用户 App - 字典数据")
+@RestController
+@RequestMapping("/system/dict-data")
+@Validated
 public class AppDictDataController {
+
+    @Resource
+    private DictDataService dictDataService;
+
+    @GetMapping("/type/{dictType}")
+    @Operation(summary = "根据字典类型查询字典数据信息")
+    public CommonResult<List<AppDictDataRespVO>> getDicts(@PathVariable String dictType) {
+        List<DictDataDO> list = dictDataService.getDictDataList(new DictDataExportReqVO().setDictType(dictType));
+        return success(DictDataConvert.INSTANCE.convertList2(list));
+    }
 }
--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/convert/dict/DictDataConvert.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/convert/dict/DictDataConvert.java
@@ -3,11 +3,11 @@ package cn.iocoder.yudao.module.system.convert.dict;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.module.system.api.dict.dto.DictDataRespDTO;
 import cn.iocoder.yudao.module.system.controller.admin.dict.vo.data.*;
+import cn.iocoder.yudao.module.system.controller.app.dict.vo.AppDictDataRespVO;
 import cn.iocoder.yudao.module.system.dal.dataobject.dict.DictDataDO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import java.util.Collection;
 import java.util.List;
 
 @Mapper
@@ -29,4 +29,5 @@ public interface DictDataConvert {
 
     DictDataRespDTO convert02(DictDataDO bean);
 
+    List<AppDictDataRespVO> convertList2(List<DictDataDO> list);
 }
