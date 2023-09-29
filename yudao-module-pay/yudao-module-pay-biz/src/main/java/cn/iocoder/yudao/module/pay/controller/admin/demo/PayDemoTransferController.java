ce6f7ab1b165fe36a197aaa996965ac7f470a650
jason <2667446@qq.com>
2023-09-29 14:01:58 +0800

    Pay: 新增转账示例单

---
+++ yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/controller/admin/demo/PayDemoTransferController.java
@@ -0,0 +1,33 @@
+package cn.iocoder.yudao.module.pay.controller.admin.demo;
+
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import cn.iocoder.yudao.module.pay.controller.admin.demo.vo.transfer.PayDemoTransferCreateReqVO;
+import cn.iocoder.yudao.module.pay.service.demo.PayDemoTransferService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+import static cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils.getLoginUserId;
+
+@Tag(name = "管理后台 - 示例转账单")
+@RestController
+@RequestMapping("/pay/demo-transfer")
+@Validated
+public class PayDemoTransferController {
+    @Resource
+    private PayDemoTransferService demoTransferService;
+
+    @PostMapping("/create")
+    @Operation(summary = "创建示例转账订单")
+    public CommonResult<Long> createDemoOrder(@Valid @RequestBody PayDemoTransferCreateReqVO createReqVO) {
+        return success(demoTransferService.createDemoTransfer(getLoginUserId(), createReqVO));
+    }
+}
