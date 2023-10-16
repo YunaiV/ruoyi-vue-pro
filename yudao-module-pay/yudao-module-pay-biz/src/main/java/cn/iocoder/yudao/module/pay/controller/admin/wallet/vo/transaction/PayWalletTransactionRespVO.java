7d0bcf5d3b5643df167aa9d751b093501dbae1b7
jason <2667446@qq.com>
2023-10-16 22:43:39 +0800

    wallet - 管理端钱包余额菜单脚本

--- yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/controller/admin/wallet/vo/transaction/PayWalletTransactionRespVO.java
+++ yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/controller/admin/wallet/vo/transaction/PayWalletTransactionRespVO.java
@@ -24,6 +24,9 @@ public class PayWalletTransactionRespVO {
     @Schema(description = "流水标题", requiredMode = Schema.RequiredMode.REQUIRED, example = "土豆土豆")
     private String title;
 
+    @Schema(description = "交易后的余额，单位分", requiredMode = Schema.RequiredMode.REQUIRED, example = "100")
+    private Long balance;
+
     @Schema(description = "交易时间", requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime createTime;
 }
