2f7371b4ea215040c2fec14d67cf84895649ba7f
puhui999 <puhui999@163.com>
2023-10-21 01:01:06 +0800

    签到记录：完善 review 提到的问题

--- yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/convert/signin/MemberSignInRecordConvert.java
+++ yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/convert/signin/MemberSignInRecordConvert.java
@@ -1,14 +1,19 @@
 package cn.iocoder.yudao.module.member.convert.signin;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjUtil;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.common.util.collection.MapUtils;
 import cn.iocoder.yudao.module.member.api.user.dto.MemberUserRespDTO;
 import cn.iocoder.yudao.module.member.controller.admin.signin.vo.record.MemberSignInRecordRespVO;
 import cn.iocoder.yudao.module.member.controller.app.signin.vo.record.AppMemberSignInRecordRespVO;
+import cn.iocoder.yudao.module.member.dal.dataobject.signin.MemberSignInConfigDO;
 import cn.iocoder.yudao.module.member.dal.dataobject.signin.MemberSignInRecordDO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 
@@ -32,10 +37,37 @@ public interface MemberSignInRecordConvert {
                 memberUserRespDTO -> record.setNickname(memberUserRespDTO.getNickname())));
         return voPageResult;
     }
+
     PageResult<MemberSignInRecordRespVO> convertPage(PageResult<MemberSignInRecordDO> pageResult);
 
     PageResult<AppMemberSignInRecordRespVO> convertPage02(PageResult<MemberSignInRecordDO> pageResult);
 
     AppMemberSignInRecordRespVO coverRecordToAppRecordVo(MemberSignInRecordDO memberSignInRecordDO);
+
+    default MemberSignInRecordDO convert(Long userId, MemberSignInRecordDO firstRecord, List<MemberSignInConfigDO> signInConfigs) {
+        // 1. 计算今天是第几天签到
+        long day = ChronoUnit.DAYS.between(firstRecord.getCreateTime(), LocalDateTime.now());
+        // 2. 初始化签到信息
+        MemberSignInRecordDO signInRecord = new MemberSignInRecordDO().setUserId(userId)
+                .setDay(Integer.parseInt(Long.toString(day))) // 设置签到天数
+                .setPoint(0)  // 设置签到积分默认为 0
+                .setExperience(0);  // 设置签到经验默认为 0
+
+
+        // 3. 获取签到对应的积分数
+        MemberSignInConfigDO lastConfig = signInConfigs.get(signInConfigs.size() - 1); // 最大签到天数
+        if (day > lastConfig.getDay()) { // 超出范围按第一天的经验计算
+            signInRecord.setPoint(signInConfigs.get(0).getPoint());
+            signInRecord.setExperience(signInConfigs.get(0).getExperience());
+            return signInRecord;
+        }
+        MemberSignInConfigDO signInConfig = CollUtil.findOne(signInConfigs, config -> ObjUtil.equal(config.getDay(), day));
+        if (signInConfig == null) {
+            return signInRecord;
+        }
+        signInRecord.setPoint(signInConfig.getPoint());
+        signInRecord.setExperience(signInConfig.getExperience());
+        return signInRecord;
+    }
 
 }
