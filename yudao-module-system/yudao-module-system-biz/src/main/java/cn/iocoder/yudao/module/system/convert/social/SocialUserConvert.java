12ae5bf128d0c405c7e907d613a8c682f90b063f
puhui999 <puhui999@163.com>
2023-10-25 16:57:13 +0800

    新增社交用户 RUD

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/convert/social/SocialUserConvert.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/convert/social/SocialUserConvert.java
@@ -1,11 +1,17 @@
 package cn.iocoder.yudao.module.system.convert.social;
 
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.module.system.api.social.dto.SocialUserBindReqDTO;
 import cn.iocoder.yudao.module.system.api.social.dto.SocialUserUnbindReqDTO;
 import cn.iocoder.yudao.module.system.controller.admin.socail.vo.SocialUserBindReqVO;
 import cn.iocoder.yudao.module.system.controller.admin.socail.vo.SocialUserUnbindReqVO;
+import cn.iocoder.yudao.module.system.controller.admin.socail.vo.user.SocialUserRespVO;
+import cn.iocoder.yudao.module.system.controller.admin.socail.vo.user.SocialUserUpdateReqVO;
+import cn.iocoder.yudao.module.system.dal.dataobject.social.SocialUserDO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface SocialUserConvert {
@@ -15,5 +21,13 @@ public interface SocialUserConvert {
     SocialUserBindReqDTO convert(Long userId, Integer userType, SocialUserBindReqVO reqVO);
 
     SocialUserUnbindReqDTO convert(Long userId, Integer userType, SocialUserUnbindReqVO reqVO);
+
+    SocialUserDO convert(SocialUserUpdateReqVO bean);
+
+    SocialUserRespVO convert(SocialUserDO bean);
+
+    List<SocialUserRespVO> convertList(List<SocialUserDO> list);
+
+    PageResult<SocialUserRespVO> convertPage(PageResult<SocialUserDO> page);
 
 }
