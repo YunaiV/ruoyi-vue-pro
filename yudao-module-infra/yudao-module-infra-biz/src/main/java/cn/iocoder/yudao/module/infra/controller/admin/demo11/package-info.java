d83e0413b881222fce3de219db018290d7abd561
YunaiV <zhijiantianya@gmail.com>
2023-11-15 21:46:10 +0800

    清理一波 demo 示例代码，准备重新生成

--- yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/controller/admin/demo11/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 代码生成示例：单表（增删改查）
- *
- * @see cn.iocoder.yudao.module.infra.enums.codegen.CodegenTemplateTypeEnum#getType()
- */
-package cn.iocoder.yudao.module.infra.controller.admin.demo11;
