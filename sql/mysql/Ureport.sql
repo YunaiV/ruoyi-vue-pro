7c43c3a2b171b148f4f607d8592729cb9fd2cd84
liuhongfeng <291117974@qq.com>
2023-11-20 23:31:21 +0800

    【新增】ureport2 集成的SQL语句

--- sql/mysql/Ureport.sql
+++ sql/mysql/Ureport.sql
@@ -53,3 +53,20 @@ VALUES (
            'Ureport2报表导出', 'report:ureport-file:export', 3, 5, @parentId,
            '', '', '', 0
        );
+
+
+DROP TABLE IF EXISTS `ureport_file`;
+CREATE TABLE `ureport_file`  (
+     `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+     `file_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文件名称',
+     `status` tinyint(4) NOT NULL COMMENT '状态',
+     `file_content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '文件内容',
+     `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '备注',
+     `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '创建者',
+     `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+     `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '更新者',
+     `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+     `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+     `tenant_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '租户编号',
+     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = 'Ureport2报表' ROW_FORMAT = Dynamic;
