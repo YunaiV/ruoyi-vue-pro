bd91276155934a0693af53a6a650387ebac3316e
YunaiV <zhijiantianya@gmail.com>
2023-11-25 20:44:36 +0800

    websocket：重新封装 websocket 组件，支持 token 认证，并增加 WebSocketMessageListener 方便处理消息

--- yudao-framework/yudao-spring-boot-starter-security/src/main/java/cn/iocoder/yudao/framework/security/core/filter/TokenAuthenticationFilter.java
+++ yudao-framework/yudao-spring-boot-starter-security/src/main/java/cn/iocoder/yudao/framework/security/core/filter/TokenAuthenticationFilter.java
@@ -76,7 +76,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             }
             // 用户类型不匹配，无权限
             // 注意：只有 /admin-api/* 和 /app-api/* 有 userType，才需要比对用户类型
-            // TODO 芋艿：ws 要不要区分开？
+            // 类似 WebSocket 的 /ws/* 连接地址，是不需要比对用户类型的
             if (userType != null
                     && ObjectUtil.notEqual(accessToken.getUserType(), userType)) {
                 throw new AccessDeniedException("错误的用户类型");
