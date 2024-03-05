f4b1e9ca4ae1a0b46cb44152c3b9fc077c4373e0
dhb52 <dhb52@126.com>
2024-03-05 15:13:53 +0000

    feat: MapUtils.findAndThen，key 为 null 时, 不处理
    Signed-off-by: dhb52 <dhb52@126.com>

--- yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/collection/MapUtils.java
+++ yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/collection/MapUtils.java
@@ -40,6 +40,7 @@ public class MapUtils {
 
     /**
      * 从哈希表查找到 key 对应的 value，然后进一步处理
+     * key 为 null 时, 不处理
      * 注意，如果查找到的 value 为 null 时，不进行处理
      *
      * @param map 哈希表
@@ -47,7 +48,7 @@ public class MapUtils {
      * @param consumer 进一步处理的逻辑
      */
     public static <K, V> void findAndThen(Map<K, V> map, K key, Consumer<V> consumer) {
-        if (CollUtil.isEmpty(map)) {
+        if (ObjUtil.isNull(key) || CollUtil.isEmpty(map)) {
             return;
         }
         V value = map.get(key);
