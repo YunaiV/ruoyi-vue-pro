e7e5b729a6712de905a1eba79dc4adcb42bbaf33
cherishsince <cherishsince@aliyun.com>
2024-04-01 22:12:23 +0800

    mj 命令类型

---
+++ yudao-module-ai/yudao-spring-boot-starter-ai/src/main/java/cn/iocoder/yudao/framework/ai/Midjourney/MjCommandEnum.java
@@ -0,0 +1,28 @@
+package cn.iocoder.yudao.framework.ai.Midjourney;
+
+import lombok.Getter;
+
+/**
+ * MJ 命令类型
+ */
+@Getter
+public enum MjCommandEnum {
+
+    IMAGINE("imagine", "生成图片"),
+    DESCRIBE("describe", "生成描述"),
+    FAST("fast", "快速生成"),
+    SETTINGS("settings", "设置"),
+    ASK("ask", "提问"),
+    BLEND("blend", "融合"),
+
+    ;
+
+    MjCommandEnum(String value, String message) {
+        this.value =value;
+        this.message =message;
+    }
+
+    private String value;
+    private String message;
+
+}
