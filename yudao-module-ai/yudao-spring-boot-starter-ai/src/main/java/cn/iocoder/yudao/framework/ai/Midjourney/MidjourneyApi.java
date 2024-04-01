73631fb364a947d047e553dcc75988b12c6d0670
cherishsince <cherishsince@aliyun.com>
2024-04-01 21:25:35 +0800

    增加注释

---
+++ yudao-module-ai/yudao-spring-boot-starter-ai/src/main/java/cn/iocoder/yudao/framework/ai/Midjourney/MidjourneyApi.java
@@ -0,0 +1,10 @@
+package cn.iocoder.yudao.framework.ai.Midjourney;
+
+/**
+ * 文档: https://www.xiubbs.com/t-401-1-1.html
+ *
+ * https://github.com/novicezk/midjourney-proxy/blob/main/README_CN.md
+ *
+ */
+public class MidjourneyApi {
+}
