e8f4dd790599dc172362235b295e4b570b297eef
cherishsince <cherishsince@aliyun.com>
2024-04-18 17:00:45 +0800

    添加对话 res 返回信息

--- yudao-module-ai/yudao-module-ai-biz/src/main/java/cn/iocoder/yudao/module/ai/vo/ChatConversationRes.java
+++ yudao-module-ai/yudao-module-ai-biz/src/main/java/cn/iocoder/yudao/module/ai/vo/ChatConversationRes.java
@@ -1,5 +1,9 @@
 package cn.iocoder.yudao.module.ai.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 /**
  * 聊天对话 res
  *
@@ -7,5 +11,28 @@ package cn.iocoder.yudao.module.ai.vo;
  * @time 2024/4/18 16:24
  * @since 1.0
  */
+@Data
+@Accessors(chain = true)
 public class ChatConversationRes {
+
+    @Schema(description = "id")
+    private Long id;
+
+    @Schema(description = "用户id")
+    private Long userId;
+
+    @Schema(description = "chat角色Id")
+    private Long chatRoleId;
+
+    @Schema(description = "chat角色名称")
+    private String chatRoleName;
+
+    @Schema(description = "聊天标题(有程序自动生成)")
+    private String chatTitle;
+
+    @Schema(description = "聊天次数(有程序自动生成)")
+    private Integer chatCount;
+
+    @Schema(description = "对话类型(roleChat、userChat)")
+    private String chatType;
 }
