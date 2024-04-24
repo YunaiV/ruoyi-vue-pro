e4dc715458d353d5b5d1eecdd2d22c322986effa
cherishsince <cherishsince@aliyun.com>
2024-04-24 16:45:58 +0800

    增加可见范围 enum

---
+++ yudao-module-ai/yudao-module-ai-api/src/main/java/cn/iocoder/yudao/module/ai/enums/ChatRoleVisibilityEnum.java
@@ -0,0 +1,25 @@
+package cn.iocoder.yudao.module.ai.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * chat角色 可见范围
+ *
+ * @author fansili
+ * @time 2024/4/24 16:44
+ * @since 1.0
+ */
+@AllArgsConstructor
+@Getter
+public enum ChatRoleVisibilityEnum {
+
+    PUBLIC("public", "公开"),
+    PRIVATE("private", "私有的"),
+
+    ;
+
+    private String type;
+
+    private String name;
+}
