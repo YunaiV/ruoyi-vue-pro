118158424b7cd152a2752468533af212854c3207
cherishsince <cherishsince@aliyun.com>
2024-05-06 15:55:43 +0800

    【优化】考虑到前端传入的json配置不确定，先不转化config

--- yudao-module-ai/yudao-module-ai-biz/src/main/java/cn/iocoder/yudao/module/ai/convert/AiChatModalConvert.java
+++ yudao-module-ai/yudao-module-ai-biz/src/main/java/cn/iocoder/yudao/module/ai/convert/AiChatModalConvert.java
@@ -4,12 +4,14 @@ import cn.iocoder.yudao.module.ai.dal.dataobject.AiChatModalDO;
 import cn.iocoder.yudao.module.ai.vo.AiChatModalAddReq;
 import cn.iocoder.yudao.module.ai.vo.AiChatModalListRes;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 /**
- * 聊天 对话 convert
+ * 聊天 modal
  *
  * @author fansili
  * @time 2024/4/18 16:39
@@ -29,10 +31,13 @@ public interface AiChatModalConvert {
     List<AiChatModalListRes> convertAiChatModalListRes(List<AiChatModalDO> list);
 
     /**
-     * 转换  - AiChatModalDO
+     * 转换 - AiChatModalDO
      *
      * @param req
      * @return
      */
+    @Mappings({
+            @Mapping(target = "config", ignore = true)
+    })
     AiChatModalDO convertAiChatModalDO(AiChatModalAddReq req);
 }
