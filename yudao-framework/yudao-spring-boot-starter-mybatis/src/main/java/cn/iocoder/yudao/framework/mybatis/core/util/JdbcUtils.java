0ae11e0eb8dc3bbdacc341cf615e92fc85d257c4
dhb52 <dhb52@126.com>
2024-06-06 23:28:55 +0800

    fix: JdbcUtils.getDbType 使用MyBatis-Plus工具

--- yudao-framework/yudao-spring-boot-starter-mybatis/src/main/java/cn/iocoder/yudao/framework/mybatis/core/util/JdbcUtils.java
+++ yudao-framework/yudao-spring-boot-starter-mybatis/src/main/java/cn/iocoder/yudao/framework/mybatis/core/util/JdbcUtils.java
@@ -35,8 +35,7 @@ public class JdbcUtils {
      * @return DB 类型
      */
     public static DbType getDbType(String url) {
-        String name = com.alibaba.druid.util.JdbcUtils.getDbType(url, null);
-        return DbType.getDbType(name);
+        return com.baomidou.mybatisplus.extension.toolkit.JdbcUtils.getDbType(url);
     }
 
 }
