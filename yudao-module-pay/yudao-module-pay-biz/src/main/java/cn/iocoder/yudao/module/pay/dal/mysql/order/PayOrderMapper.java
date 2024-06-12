643a50f7a044aa1acf2278278c716e7ab21ff9af
DevDengChao <2325690622@qq.com>
2024-06-12 14:04:18 +0800

    feat: export pay order page by user id

--- yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/dal/mysql/order/PayOrderMapper.java
+++ yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/dal/mysql/order/PayOrderMapper.java
@@ -31,6 +31,7 @@ public interface PayOrderMapper extends BaseMapperX<PayOrderDO> {
     default List<PayOrderDO> selectList(PayOrderExportReqVO reqVO) {
         return selectList(new LambdaQueryWrapperX<PayOrderDO>()
                 .eqIfPresent(PayOrderDO::getAppId, reqVO.getAppId())
+                .eqIfPresent(PayOrderDO::getUserId, reqVO.getUserId())
                 .eqIfPresent(PayOrderDO::getChannelCode, reqVO.getChannelCode())
                 .likeIfPresent(PayOrderDO::getMerchantOrderId, reqVO.getMerchantOrderId())
                 .likeIfPresent(PayOrderDO::getChannelOrderNo, reqVO.getChannelOrderNo())
