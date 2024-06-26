9fef49d8b34b825ea54a8b9331e8fb70d32d0b80
cherishsince <cherishsince@aliyun.com>
2024-06-26 15:49:30 +0800

    【解决todo】AiModelEnum 已解决

--- yudao-module-ai/yudao-module-ai-api/src/main/java/cn/iocoder/yudao/module/ai/enums/model/AiModelEnum.java
+++ yudao-module-ai/yudao-module-ai-api/src/main/java/cn/iocoder/yudao/module/ai/enums/model/AiModelEnum.java
@@ -3,9 +3,9 @@ package cn.iocoder.yudao.module.ai.enums.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-// TODO done @fansili：1）类注释要加下；2）author 和 time 用 javadoc，@author 和 @since；3）@AllArgsConstructor 使用这个注解，去掉构造方法；4）value 改成 model 字段，然后注释都写下哈；5）message 改成 name，然后注释都写下哈
-// TODO @fan: AiModelEnum 是不是可以缩写成这个哈；所有的模型，都写在这里枚举；
 /**
+ * ai 模型
+ *
  * @author: fansili
  * @time: 2024/3/4 12:36
  */
