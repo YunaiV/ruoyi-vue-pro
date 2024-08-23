94effa87a302dd04f12356b2632c7df3147056a2
卢越 <552369664@qq.com>
2024-08-23 16:32:25 +0800

    【功能优化】订单日志根据创建时间倒序

--- yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/dal/mysql/aftersale/AfterSaleLogMapper.java
+++ yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/dal/mysql/aftersale/AfterSaleLogMapper.java
@@ -2,6 +2,7 @@ package cn.iocoder.yudao.module.trade.dal.mysql.aftersale;
 
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
 import cn.iocoder.yudao.module.trade.dal.dataobject.aftersale.AfterSaleLogDO;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,7 +11,10 @@ import java.util.List;
 public interface AfterSaleLogMapper extends BaseMapperX<AfterSaleLogDO> {
 
     default List<AfterSaleLogDO> selectListByAfterSaleId(Long afterSaleId) {
-        return selectList(AfterSaleLogDO::getAfterSaleId, afterSaleId);
+        LambdaQueryWrapper<AfterSaleLogDO> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(AfterSaleLogDO::getAfterSaleId, afterSaleId);
+        queryWrapper.orderByDesc(AfterSaleLogDO::getCreateTime);
+        return selectList(queryWrapper);
     }
 
 }
