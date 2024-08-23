427cf8e4f6dfc63913e3830efd56dfd8683da28c
卢越 <552369664@qq.com>
2024-08-23 16:42:06 +0800

    【功能优化】订单日志根据创建时间倒序

--- yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/dal/mysql/order/TradeOrderLogMapper.java
+++ yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/dal/mysql/order/TradeOrderLogMapper.java
@@ -2,6 +2,7 @@ package cn.iocoder.yudao.module.trade.dal.mysql.order;
 
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
 import cn.iocoder.yudao.module.trade.dal.dataobject.order.TradeOrderLogDO;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,7 +11,10 @@ import java.util.List;
 public interface TradeOrderLogMapper extends BaseMapperX<TradeOrderLogDO> {
 
     default List<TradeOrderLogDO> selectListByOrderId(Long orderId) {
-        return selectList(TradeOrderLogDO::getOrderId, orderId);
+        LambdaQueryWrapper<TradeOrderLogDO> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(TradeOrderLogDO::getOrderId, orderId);
+        queryWrapper.orderByDesc(TradeOrderLogDO::getCreateTime);
+        return selectList(queryWrapper);
     }
 
 }
