12ce87b3056718a28d2f5d9c0e5344bab6dacc8a
YunaiV <zhijiantianya@gmail.com>
2024-09-06 21:43:59 +0800

    【功能修复】框架：操作日志异步记录时，丢失 request 的问题

--- yudao-module-system/yudao-module-system-api/src/main/java/cn/iocoder/yudao/module/system/api/logger/OperateLogApi.java
+++ yudao-module-system/yudao-module-system-api/src/main/java/cn/iocoder/yudao/module/system/api/logger/OperateLogApi.java
@@ -5,6 +5,7 @@ import cn.iocoder.yudao.module.system.api.logger.dto.OperateLogCreateReqDTO;
 import cn.iocoder.yudao.module.system.api.logger.dto.OperateLogPageReqDTO;
 import cn.iocoder.yudao.module.system.api.logger.dto.OperateLogRespDTO;
 import jakarta.validation.Valid;
+import org.springframework.scheduling.annotation.Async;
 
 /**
  * 操作日志 API 接口
@@ -19,6 +20,16 @@ public interface OperateLogApi {
      * @param createReqDTO 请求
      */
     void createOperateLog(@Valid OperateLogCreateReqDTO createReqDTO);
+
+    /**
+     * 【异步】创建操作日志
+     *
+     * @param createReqDTO 请求
+     */
+    @Async
+    default void createOperateLogAsync(OperateLogCreateReqDTO createReqDTO) {
+        createOperateLog(createReqDTO);
+    }
 
     /**
      * 获取指定模块的指定数据的操作日志分页
