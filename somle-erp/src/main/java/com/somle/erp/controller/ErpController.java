b581509dc76805c67fa3c141cd78a1e4d59d468b
c-tao <taoc97@outlook.com>
2024-09-07 16:45:56 +0800

    test change /api to /admin-api

--- somle-erp/src/main/java/com/somle/erp/controller/ErpController.java
+++ somle-erp/src/main/java/com/somle/erp/controller/ErpController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 // @Validated
 @RestController
-@RequestMapping("/api/erp")
+@RequestMapping("/admin-api/erp")
 public class ErpController {
 
     @Autowired
