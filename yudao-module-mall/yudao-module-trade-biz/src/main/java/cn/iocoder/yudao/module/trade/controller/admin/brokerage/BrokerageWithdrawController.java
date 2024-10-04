ae1cf9e098d2a53d16970486897bac494744a560
痴货 <252048765@qq.com>
2024-10-04 10:22:34 +0800

    【功能优化】增加转账回调接口

--- yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/controller/admin/brokerage/BrokerageWithdrawController.java
+++ yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/controller/admin/brokerage/BrokerageWithdrawController.java
@@ -4,6 +4,7 @@ import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.module.member.api.user.MemberUserApi;
 import cn.iocoder.yudao.module.member.api.user.dto.MemberUserRespDTO;
+import cn.iocoder.yudao.module.pay.api.notify.dto.PayTransferNotifyReqDTO;
 import cn.iocoder.yudao.module.trade.controller.admin.brokerage.vo.withdraw.BrokerageWithdrawRejectReqVO;
 import cn.iocoder.yudao.module.trade.controller.admin.brokerage.vo.withdraw.BrokerageWithdrawPageReqVO;
 import cn.iocoder.yudao.module.trade.controller.admin.brokerage.vo.withdraw.BrokerageWithdrawRespVO;
@@ -14,6 +15,8 @@ import cn.iocoder.yudao.module.trade.service.brokerage.BrokerageWithdrawService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.security.PermitAll;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +33,7 @@ import static cn.iocoder.yudao.framework.common.util.servlet.ServletUtils.getCli
 @RestController
 @RequestMapping("/trade/brokerage-withdraw")
 @Validated
+@Slf4j
 public class BrokerageWithdrawController {
 
     @Resource
@@ -74,6 +78,16 @@ public class BrokerageWithdrawController {
         Map<Long, MemberUserRespDTO> userMap = memberUserApi.getUserMap(
                 convertSet(pageResult.getList(), BrokerageWithdrawDO::getUserId));
         return success(BrokerageWithdrawConvert.INSTANCE.convertPage(pageResult, userMap));
+    }
+
+    @PostMapping("/update-transfer")
+    @Operation(summary = "更新转账订单为转账成功") // 由 pay-module 支付服务，进行回调，可见 PayNotifyJob
+    @PermitAll // 无需登录，安全由 PayDemoOrderService 内部校验实现
+    public CommonResult<Boolean> updateAfterRefund(@RequestBody PayTransferNotifyReqDTO notifyReqDTO) {
+        // 目前业务逻辑，不需要做任何事情
+        // 当然，退款会有小概率会失败的情况，可以监控失败状态，进行告警
+        log.info("[updateAfterRefund][notifyReqDTO({})]", notifyReqDTO);
+        return success(true);
     }
 
 }
