c0aeac9157dc01c4029ca94995fbd326eaaa2d09
c-tao <taoc97@outlook.com>
2024-10-12 16:17:06 +0800

    add eccang order create and pay data job as stand alone job

---
+++ somle-esb/src/main/java/com/somle/esb/job/EccangOrderPlatformPayDataJob.java
@@ -0,0 +1,50 @@
+package com.somle.esb.job;
+
+
+import com.somle.eccang.model.EccangOrderVO;
+import com.somle.eccang.service.EccangService;
+import com.somle.esb.model.Domain;
+import com.somle.esb.model.OssData;
+import com.somle.esb.service.EsbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EccangOrderPlatformPayDataJob extends DataJob{
+    @Autowired
+    EsbService service;
+
+    @Autowired
+    EccangService eccangService;
+
+    final String DATABASE = Domain.ECCANG.toString();
+
+
+    @Override
+    public String execute(String param) throws Exception {
+        setDate(param);
+
+        eccangService.getOrderPages(
+                EccangOrderVO.builder()
+                    .platformPaidDateStart(yesterdayFirstSecond)
+                    .platformPaidDateEnd(yesterdayLastSecond)
+                    .build()
+            )
+            .forEach(page -> {
+                OssData data = OssData.builder()
+                    .database(DATABASE)
+                    .tableName("order_platform_pay")
+                    .syncType("inc")
+                    .requestTimestamp(System.currentTimeMillis())
+                    .folderDate(yesterday)
+                    .content(page)
+                    .headers(null)
+                    .build();
+                service.send(data);
+            });
+
+        
+        
+        return "data upload success";
+    }
+}
