ac4d1e24c3a1b91d8920c600813672f010825654
c-tao <schwarzft@gmail.com>
2024-11-05 17:32:21 +0800

    main/fix: change wangdian order datajob to trade

---
+++ somle-esb/src/main/java/com/somle/esb/job/WangdianTradeDataJob.java
@@ -0,0 +1,39 @@
+package com.somle.esb.job;
+
+
+import com.somle.esb.model.OssData;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class WangdianTradeDataJob extends WangdianDataJob{
+
+
+    @Override
+    public String execute(String param) throws Exception {
+        setDate(param);
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("start_time", yesterdayFirstSecond.toString());
+        params.put("end_time", yesterdayLastSecond.toString());
+        params.put("page_size", "30");
+        params.put("page_no", "0");
+
+        var result = wangdianService.client.execute("trade_query.php", params);
+
+        var data = OssData.builder()
+                .database(DATABASE)
+                .tableName("trade")
+                .syncType("inc")
+                .requestTimestamp(System.currentTimeMillis())
+                .folderDate(yesterday)
+                .content(result)
+                .headers(null)
+                .build();
+        service.send(data);
+
+        throw new Exception("data upload success");
+    }
+}
