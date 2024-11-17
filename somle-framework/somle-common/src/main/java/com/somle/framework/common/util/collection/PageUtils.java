ab7bdd0437a0dd5f65039f52beb426a41f035435
c-tao <schwarzft@gmail.com>
2024-11-17 18:46:04 +0800

    main/feature: PageUtils wip

---
+++ somle-framework/somle-common/src/main/java/com/somle/framework/common/util/collection/PageUtils.java
@@ -0,0 +1,78 @@
+package com.somle.framework.common.util.collection;
+
+/*
+ * @Description: $
+ * @Author: c-tao
+ * @Date: $
+ */
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class PageUtils {
+
+    /**
+     * Fetches all pages of results starting from the first page as a Stream.
+     *
+     * @param <T>          The type representing a page of results.
+     * @param firstPage    The first page of results.
+     * @param isLastPage   A method to decide if the current page is the last page.
+     * @param getNextPage  A method to produce the next page given the current page.
+     * @return A Stream of all pages of results.
+     */
+    public static <T> Stream<T> getAllPages(
+        T firstPage,
+        Predicate<T> isLastPage,
+        Function<T, T> getNextPage
+    ) {
+        return StreamSupport.stream(new PaginationSpliterator<>(firstPage, isLastPage, getNextPage), false);
+    }
+}
+
+
+
+class PaginationSpliterator<T> implements Spliterator<T> {
+    private T currentPage;
+    private final Predicate<T> isLastPage;
+    private final Function<T, T> getNextPage;
+    private boolean finished = false;
+
+    public PaginationSpliterator(T firstPage, Predicate<T> isLastPage, Function<T, T> getNextPage) {
+        this.currentPage = firstPage;
+        this.isLastPage = isLastPage;
+        this.getNextPage = getNextPage;
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super T> action) {
+        if (finished) {
+            return false;
+        }
+        action.accept(currentPage);
+        if (isLastPage.test(currentPage)) {
+            finished = true;
+        } else {
+            currentPage = getNextPage.apply(currentPage);
+        }
+        return true;
+    }
+
+    @Override
+    public Spliterator<T> trySplit() {
+        return null; // Not supporting parallel streams
+    }
+
+    @Override
+    public long estimateSize() {
+        return Long.MAX_VALUE; // Unknown size
+    }
+
+    @Override
+    public int characteristics() {
+        return ORDERED | NONNULL | IMMUTABLE;
+    }
+}
