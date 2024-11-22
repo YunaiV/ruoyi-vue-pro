934e5ac34ed2c18cfaacf6a13f39508e4b3c8c7e
c-tao <schwarzft@gmail.com>
2024-11-22 14:11:13 +0800

    main/fix: fix WangdianRespVO have builder

--- somle-wangdian/src/main/java/com/somle/wangdian/model/WangdianRespVO.java
+++ somle-wangdian/src/main/java/com/somle/wangdian/model/WangdianRespVO.java
@@ -8,11 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
--- somle-wangdian/src/main/java/com/somle/wangdian/model/WangdianTradeRespVO.java
+++ somle-wangdian/src/main/java/com/somle/wangdian/model/WangdianTradeRespVO.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
