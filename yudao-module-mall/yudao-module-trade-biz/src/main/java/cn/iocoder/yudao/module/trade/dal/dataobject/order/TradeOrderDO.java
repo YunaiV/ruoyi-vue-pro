424750f62849c629ac5c06c12f95363d82f79815
puhui999 <puhui999@163.com>
2024-11-29 14:49:28 +0800

    【缺陷修复】商城：赠送的优惠劵相关转换

--- yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/dal/dataobject/order/TradeOrderDO.java
+++ yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/dal/dataobject/order/TradeOrderDO.java
@@ -27,7 +27,7 @@ import java.util.Map;
  *
  * @author 芋道源码
  */
-@TableName("trade_order")
+@TableName(value = "trade_order", autoResultMap = true)
 @KeySequence("trade_order_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
 @Data
 @EqualsAndHashCode(callSuper = true)
