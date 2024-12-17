53063d69aee9069f98ec2a9dfea79f4aa1401d75
东宇 <992484215@qq.com>
2024-12-17 16:51:31 +0800

    fix: 修改AmazonspFBAReturnReportDataJob

---
+++ somle-esb/src/main/java/com/somle/esb/job/AmazonspFBAReturnReportDataJob.java
@@ -0,0 +1,61 @@
+package com.somle.esb.job;
+
+import com.somle.amazon.controller.vo.AmazonSpReportSaveVO;
+import com.somle.esb.model.OssData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class AmazonspFBAReturnReportDataJob extends AmazonspDataJob {
+
+
+    @Override
+    public String execute(String param) throws Exception {
+        setDate(param);
+
+        AmazonSpReportSaveVO vo = AmazonSpReportSaveVO.builder()
+                .reportType("GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA")
+                .dataStartTime(beforeYesterdayFirstSecond.toString())
+                .dataEndTime(beforeYesterdayLastSecond.toString())
+                .build();
+
+        amazonService.account.getSellers().stream()
+                .flatMap(seller ->
+                        seller.getShops().stream()
+                                .map(shop -> {
+                                    vo.setMarketplaceIds(Collections.singletonList(shop.getCountry().getMarketplaceId()));
+                                    return amazonService.spClient.createAndGetReport(seller, vo, String.valueOf(false));
+                                })
+                )
+                .forEach(report -> {
+
+                    OssData data = OssData.builder()
+                            .database(DATABASE)
+                            .tableName("fba_return_report")
+                            .syncType("inc")
+                            .requestTimestamp(System.currentTimeMillis())
+                            .folderDate(beforeYesterday)
+                            .content(report)
+                            .headers(getHeaders(vo))
+                            .build();
+
+                    service.send(data);
+                });
+
+        return "data upload success";
+    }
+
+    private static Map<String, String> getHeaders(AmazonSpReportSaveVO vo) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("gReportType", vo.getReportType());
+        headers.put("marketplaceIds", vo.getMarketplaceIds().toString());
+        headers.put("dataStartTime", vo.getDataStartTime());
+        headers.put("dataEndTime", vo.getDataEndTime());
+        return headers;
+    }
+}
