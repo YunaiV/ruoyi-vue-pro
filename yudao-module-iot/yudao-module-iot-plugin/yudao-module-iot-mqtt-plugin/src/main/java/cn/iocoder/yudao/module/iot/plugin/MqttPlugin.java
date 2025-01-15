890d304340f1d20c52cf5288d47956eaed5814ee
安浩浩 <1036606149@qq.com>
2025-01-15 22:37:07 +0800

    【功能完善】IoT: 更新 Vert.x 版本至 4.5.1，新增 EMQX 插件及其相关配置，重构 MQTT 插件以支持 Vert.x MQTT 服务器，优化插件启动和停止逻辑，更新插件描述信息。

--- yudao-module-iot/yudao-module-iot-plugin/yudao-module-iot-mqtt-plugin/src/main/java/cn/iocoder/yudao/module/iot/plugin/MqttPlugin.java
+++ yudao-module-iot/yudao-module-iot-plugin/yudao-module-iot-mqtt-plugin/src/main/java/cn/iocoder/yudao/module/iot/plugin/MqttPlugin.java
@@ -1,45 +1,36 @@
 package cn.iocoder.yudao.module.iot.plugin;
 
-import cn.iocoder.yudao.module.iot.api.ServiceRegistry;
-import cn.iocoder.yudao.module.iot.api.device.DeviceDataApi;
 import lombok.extern.slf4j.Slf4j;
 import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
 
-import javax.annotation.Resource;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 @Slf4j
 public class MqttPlugin extends Plugin {
 
-    private ExecutorService executorService;
-    @Resource
-    private DeviceDataApi deviceDataApi;
+    private MqttServerExtension mqttServerExtension;
 
     public MqttPlugin(PluginWrapper wrapper) {
         super(wrapper);
-        this.executorService = Executors.newSingleThreadExecutor();
     }
 
     @Override
     public void start() {
-        log.info("MqttPlugin.start()");
-
-        if (executorService.isShutdown() || executorService.isTerminated()) {
-            executorService = Executors.newSingleThreadExecutor();
-        }
-
-        deviceDataApi = ServiceRegistry.getService(DeviceDataApi.class);
-        if (deviceDataApi == null) {
-            log.error("未能从 ServiceRegistry 获取 DeviceDataApi 实例，请确保主程序已正确注册！");
-            return;
-        }
-
+        log.info("MQTT Plugin started.");
+        mqttServerExtension = new MqttServerExtension();
+        mqttServerExtension.startMqttServer();
     }
 
     @Override
     public void stop() {
-        log.info("MqttPlugin.stop()");
+        log.info("MQTT Plugin stopped.");
+        if (mqttServerExtension != null) {
+            mqttServerExtension.stopMqttServer().onComplete(ar -> {
+                if (ar.succeeded()) {
+                    log.info("Stopped MQTT Server successfully");
+                } else {
+                    log.error("Failed to stop MQTT Server: {}", ar.cause().getMessage());
+                }
+            });
+        }
     }
 }
