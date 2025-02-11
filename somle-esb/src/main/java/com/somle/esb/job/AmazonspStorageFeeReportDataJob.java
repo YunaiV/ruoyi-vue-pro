7de992024c2492382a50b4f7f8fc6c45aaa154c2
c-tao <schwarzft@gmail.com>
2025-02-11 13:23:51 +0800

    fix: 修正AmazonspStorageFeeReportDataJob未填marketplaceIds的问题

--- somle-esb/src/main/java/com/somle/esb/job/AmazonspStorageFeeReportDataJob.java
+++ somle-esb/src/main/java/com/somle/esb/job/AmazonspStorageFeeReportDataJob.java
@@ -3,6 +3,7 @@ package com.somle.esb.job;
 
 import com.somle.amazon.controller.vo.AmazonSpReportReqVO;
 import com.somle.amazon.controller.vo.AmazonSpReportSaveVO;
+import com.somle.amazon.model.enums.AmazonCountry;
 import com.somle.esb.model.OssData;
 import com.somle.framework.common.util.csv.TsvUtils;
 import org.springframework.stereotype.Component;
@@ -17,28 +18,30 @@ public class AmazonspStorageFeeReportDataJob extends AmazonspDataJob {
     public String execute(String param) throws Exception {
         setDate(param);
 
-        var vo = AmazonSpReportSaveVO.builder()
-                .reportType("GET_FBA_STORAGE_FEE_CHARGES_DATA")
-                .dataStartTime(beforeYesterdayFirstSecond.toString())
-                .dataEndTime(beforeYesterdayLastSecond.toString())
-                .build();
+        for (var client : amazonSpService.clients) {
+            client.getMarketplaceParticipations().stream()
+                .forEach(marketplaceParticipation -> {
+                    var vo = AmazonSpReportSaveVO.builder()
+                        .reportType("GET_FBA_STORAGE_FEE_CHARGES_DATA")
+                        .marketplaceIds(List.of(marketplaceParticipation.getMarketplace().getId()))
+                        .dataStartTime(beforeYesterdayFirstSecond.toString())
+                        .dataEndTime(beforeYesterdayLastSecond.toString())
+                        .build();
+                    var report = client.createAndGetReportOrNull(vo, "gzip");
+                    OssData data = OssData.builder()
+                        .database(DATABASE)
+                        .tableName("storage_fee_report")
+                        .syncType("inc")
+                        .requestTimestamp(System.currentTimeMillis())
+                        .folderDate(beforeYesterday)
+                        .content(report != null ? TsvUtils.toMapList(report) : "")
+                        .headers(null)
+                        .build();
+                    service.send(data);
+                });
 
-        amazonSpService.clients.stream()
-            .map(client ->
-                client.createAndGetReportOrNull(vo, "gzip")
-            )
-            .forEach(report -> {
-                OssData data = OssData.builder()
-                    .database(DATABASE)
-                    .tableName("storage_fee_report")
-                    .syncType("inc")
-                    .requestTimestamp(System.currentTimeMillis())
-                    .folderDate(beforeYesterday)
-                    .content(report != null ? TsvUtils.toMapList(report) : "")
-                    .headers(null)
-                    .build();
-                service.send(data);
-            });
+        }
         return "data upload success";
+
     }
 }
