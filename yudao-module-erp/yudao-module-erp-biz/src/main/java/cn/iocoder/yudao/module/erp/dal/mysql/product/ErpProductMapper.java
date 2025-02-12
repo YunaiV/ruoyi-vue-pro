b93d0241f0d2bfe3f85f7e363cd7a2e373950654
fish steak <45394169+fishSteak@users.noreply.github.com>
2025-02-12 14:06:36 +0800

    erp产品的查询增加 查询入参 (#48)
    * fix 状态2字段修改
    
    * fix mapper修改
    
    * fix 采修改查询逻辑、前端去查用户id

--- yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/dal/mysql/product/ErpProductMapper.java
+++ yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/dal/mysql/product/ErpProductMapper.java
@@ -1,13 +1,17 @@
 package cn.iocoder.yudao.module.erp.dal.mysql.product;
 
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
-import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.iocoder.yudao.framework.mybatis.core.query.MPJLambdaWrapperX;
 import cn.iocoder.yudao.module.erp.controller.admin.product.vo.product.ErpProductPageReqVO;
 import cn.iocoder.yudao.module.erp.dal.dataobject.product.ErpProductDO;
+import com.github.yulichang.wrapper.MPJLambdaWrapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * ERP 产品 Mapper
@@ -18,16 +22,34 @@ import java.util.List;
 public interface ErpProductMapper extends BaseMapperX<ErpProductDO> {
 
     default PageResult<ErpProductDO> selectPage(ErpProductPageReqVO reqVO) {
-        return selectPage(reqVO, new LambdaQueryWrapperX<ErpProductDO>()
-            .likeIfPresent(ErpProductDO::getName, reqVO.getName())
-            .eqIfPresent(ErpProductDO::getCategoryId, reqVO.getCategoryId())
-            .likeIfPresent(ErpProductDO::getBarCode, reqVO.getBarCode())
-            .likeIfPresent(ErpProductDO::getBrand, reqVO.getBrand())
-            .eqIfPresent(ErpProductDO::getDeptId, reqVO.getDeptId())
-            .likeIfPresent(ErpProductDO::getSeries, reqVO.getSeries())
-            .eqIfPresent(ErpProductDO::getStatus, reqVO.getStatus())
-            .betweenIfPresent(ErpProductDO::getCreateTime, reqVO.getCreateTime())
-            .orderByDesc(ErpProductDO::getId));
+        MPJLambdaWrapper<ErpProductDO> orderByDesc = new MPJLambdaWrapperX<ErpProductDO>()
+            .betweenIfPresent(ErpProductDO::getCreateTime, reqVO.getCreateTime()) // 添加创建时间查询
+            .betweenIfPresent(ErpProductDO::getUpdateTime, reqVO.getUpdateTime())  // 添加修改时间查询
+//            .leftJoin(AdminUserDO.class, "creatorUser", AdminUserDO::getId, ErpProductDO::getCreator)
+//            .leftJoin(AdminUserDO.class, "updaterUser", AdminUserDO::getId, ErpProductDO::getUpdater)
+            .likeIfExists(ErpProductDO::getName, reqVO.getName())
+            .eqIfExists(ErpProductDO::getCategoryId, reqVO.getCategoryId())
+            .likeIfExists(ErpProductDO::getBarCode, reqVO.getBarCode())
+            .likeIfExists(ErpProductDO::getBrand, reqVO.getBrand())
+            .eqIfExists(ErpProductDO::getDeptId, reqVO.getDeptId())
+            .likeIfExists(ErpProductDO::getSeries, reqVO.getSeries())
+            .eqIfExists(ErpProductDO::getStatus, reqVO.getStatus())
+            .likeIfExists(ErpProductDO::getPackageWidth, reqVO.getPackageWidth())  // 包装宽度查询
+            .likeIfExists(ErpProductDO::getPackageLength, reqVO.getPackageLength())  // 包装长度查询
+            .likeIfExists(ErpProductDO::getPackageHeight, reqVO.getPackageHeight())  // 包装高度查询
+            .likeIfExists(ErpProductDO::getPackageWeight, reqVO.getPackageWeight())  // 包装重量查询
+            .likeIfExists(ErpProductDO::getWidth, reqVO.getWidth())  // 宽度查询
+            .likeIfExists(ErpProductDO::getLength, reqVO.getLength()) // 长度查询
+            .likeIfExists(ErpProductDO::getHeight, reqVO.getHeight())  // 高度查询
+            .likeIfExists(ErpProductDO::getWeight, reqVO.getWeight())  // 重量查询
+            // 创建人 模糊
+            .eqIfExists(ErpProductDO::getCreator, reqVO.getCreator())
+//            .likeIfExists("creatorUser", AdminUserDO::getNickname, reqVO.getCreator())
+            // 更新人 模糊
+            .eqIfExists(ErpProductDO::getUpdater, reqVO.getUpdater())
+//            .likeIfExists("updaterUser", AdminUserDO::getNickname, reqVO.getUpdater())
+            .orderByDesc(ErpProductDO::getId);
+        return selectPage(reqVO, orderByDesc);
     }
 
     default Long selectCountByCategoryId(Long categoryId) {
@@ -47,27 +69,27 @@ public interface ErpProductMapper extends BaseMapperX<ErpProductDO> {
     }
 
     /**
+     * @return java.lang.Integer
      * @Author Wqh
      * @Description 根据颜色，系列，型号查询出最大的流水号
      * @Date 13:36 2024/10/21
      * @Param [barCode]
-     * @return java.lang.Integer
      **/
     default ErpProductDO selectMaxSerialByColorAndModelAndSeries(String color, String model, String series) {
         return selectOne(new LambdaQueryWrapperX<ErpProductDO>()
-                .eqIfPresent(ErpProductDO::getColor, color)
-                .eqIfPresent(ErpProductDO::getModel, model)
-                .eqIfPresent(ErpProductDO::getSeries, series)
-                .orderByDesc(ErpProductDO::getSerial)
-                .select(ErpProductDO::getSerial)
-                .last("limit 1"));
+            .eqIfPresent(ErpProductDO::getColor, color)
+            .eqIfPresent(ErpProductDO::getModel, model)
+            .eqIfPresent(ErpProductDO::getSeries, series)
+            .orderByDesc(ErpProductDO::getSerial)
+            .select(ErpProductDO::getSerial)
+            .last("limit 1"));
     }
 
-    default List<ErpProductDO> selectByColorAndSeriesAndModel(String color, String model, String series){
+    default List<ErpProductDO> selectByColorAndSeriesAndModel(String color, String model, String series) {
         return selectList(new LambdaQueryWrapperX<ErpProductDO>()
-                .eqIfPresent(ErpProductDO::getColor, color)
-                .eqIfPresent(ErpProductDO::getModel, model)
-                .eqIfPresent(ErpProductDO::getSeries, series));
+            .eqIfPresent(ErpProductDO::getColor, color)
+            .eqIfPresent(ErpProductDO::getModel, model)
+            .eqIfPresent(ErpProductDO::getSeries, series));
     }
 
     default ErpProductDO selectByName(String name) {
@@ -76,13 +98,14 @@ public interface ErpProductMapper extends BaseMapperX<ErpProductDO> {
 
     /**
      * 根据条码(barCode)查询出id列表
-     * @param barCode  SKU（编码
+     *
+     * @param barCode SKU（编码
      * @return id集合
      */
     default List<Long> selectIdListByBarCode(String barCode) {
         return selectList(new LambdaQueryWrapperX<ErpProductDO>()
-                .like(ErpProductDO::getBarCode, barCode)
-                .select(ErpProductDO::getId))
-                .stream().map(ErpProductDO::getId).toList();
+            .like(ErpProductDO::getBarCode, barCode)
+            .select(ErpProductDO::getId))
+            .stream().map(ErpProductDO::getId).toList();
     }
 }
