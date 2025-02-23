c96d966a41622bd41d3dbbfad6062e66689d64c4
YunaiV <zhijiantianya@gmail.com>
2025-02-23 17:57:59 +0800

    【代码优化】AI：无法适配 Spring AI 1.0.6 对百度 QianFan 的逻辑：无语！！！

--- yudao-module-ai/yudao-spring-boot-starter-ai/src/test/java/cn/iocoder/yudao/framework/ai/image/QianFanImageTests.java
+++ yudao-module-ai/yudao-spring-boot-starter-ai/src/test/java/cn/iocoder/yudao/framework/ai/image/QianFanImageTests.java
@@ -10,14 +10,15 @@ import org.springframework.ai.qianfan.api.QianFanImageApi;
 
 import static cn.iocoder.yudao.framework.ai.image.StabilityAiImageModelTests.viewImage;
 
+// TODO @芋艿：百度千帆 API 提供了 V2 版本，目前 Spring AI 不兼容，可关键 <https://github.com/spring-projects/spring-ai/issues/2179> 进展
+
 /**
  * {@link QianFanImageModel} 集成测试类
  */
 public class QianFanImageTests {
 
-    private final QianFanImageApi imageApi = new QianFanImageApi(
-            "qS8k8dYr2nXunagK4SSU8Xjj", "pHGbx51ql2f0hOyabQvSZezahVC3hh3e");
-    private final QianFanImageModel imageModel = new QianFanImageModel(imageApi);
+    private final QianFanImageModel imageModel = new QianFanImageModel(
+            new QianFanImageApi("qS8k8dYr2nXunagK4SSU8Xjj", "pHGbx51ql2f0hOyabQvSZezahVC3hh3e")); // 密钥
 
     @Test
     @Disabled
