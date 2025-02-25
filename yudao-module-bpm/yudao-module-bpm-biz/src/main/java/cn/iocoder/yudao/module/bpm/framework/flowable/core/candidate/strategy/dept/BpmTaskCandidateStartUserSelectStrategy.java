b2ca263067661919b0eeec181058b228bfd45593
lizhixian <18210040298@163.com>
2025-02-25 16:29:35 +0800

    feat：添加描述

--- yudao-module-bpm/yudao-module-bpm-biz/src/main/java/cn/iocoder/yudao/module/bpm/framework/flowable/core/candidate/strategy/dept/BpmTaskCandidateStartUserSelectStrategy.java
+++ yudao-module-bpm/yudao-module-bpm-biz/src/main/java/cn/iocoder/yudao/module/bpm/framework/flowable/core/candidate/strategy/dept/BpmTaskCandidateStartUserSelectStrategy.java
@@ -53,7 +53,7 @@ public class BpmTaskCandidateStartUserSelectStrategy extends AbstractBpmTaskCand
         Map<String, List<Long>> startUserSelectAssignees = FlowableUtils.getStartUserSelectAssignees(processInstance);
         Assert.notNull(startUserSelectAssignees, "流程实例({}) 的发起人自选审批人不能为空",
                 execution.getProcessInstanceId());
-        // 获得审批人，如果不存在，则直接返回空，避免类型转换异常
+        // 获得审批人，如果不存在，则直接返回空，fix: 用于节点预测时，如果该节点不存在发起人自选审批人，类型转换异常
         List<Long> assignees = startUserSelectAssignees.get(execution.getCurrentActivityId());
         if (CollUtil.isEmpty(assignees)){
             return Sets.newLinkedHashSet();
@@ -71,7 +71,7 @@ public class BpmTaskCandidateStartUserSelectStrategy extends AbstractBpmTaskCand
         if (startUserSelectAssignees == null) {
             return Sets.newLinkedHashSet();
         }
-        // 获得审批人，如果不存在，则直接返回空，避免类型转换异常
+        // 获得审批人，如果不存在，则直接返回空，fix: 用于节点预测时，如果该节点不存在发起人自选审批人，类型转换异常
         List<Long> assignees = startUserSelectAssignees.get(activityId);
         if (CollUtil.isEmpty(assignees)){
             return Sets.newLinkedHashSet();
