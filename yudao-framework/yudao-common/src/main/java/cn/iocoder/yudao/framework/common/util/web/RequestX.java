5a2d4e3e1eb7bde9878372a1d2522b4098b0471e
LeeFJ <leefangjie@qq.com>
2025-03-07 16:27:14 +0800

    update WebUtils 支持URLPatten报文提交 以及 支持组织架构层级

--- yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/web/RequestX.java
+++ yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/web/RequestX.java
@@ -21,6 +21,7 @@ public class RequestX {
 //    private List<String> paths;
     private Object queryParams;
     private Map<String, String> headers;
+    @Builder.Default
     private ContentType contentType=ContentType.JSON;
     private Object payload;
 
