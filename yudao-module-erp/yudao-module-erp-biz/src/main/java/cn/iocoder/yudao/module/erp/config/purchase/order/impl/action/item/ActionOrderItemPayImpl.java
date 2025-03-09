f9110966b4230996bd0266e399d2adf73c357f8b
fishSteak <cutevillainrole@gmail.com>
2025-03-09 14:03:44 +0800

    fix 修改付款状态传递事件

--- yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/config/purchase/order/impl/action/item/ActionOrderItemPayImpl.java
+++ yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/config/purchase/order/impl/action/item/ActionOrderItemPayImpl.java
@@ -1,19 +1,30 @@
 package cn.iocoder.yudao.module.erp.config.purchase.order.impl.action.item;
 
+import cn.iocoder.yudao.module.erp.dal.dataobject.purchase.ErpPurchaseOrderDO;
 import cn.iocoder.yudao.module.erp.dal.dataobject.purchase.ErpPurchaseOrderItemDO;
 import cn.iocoder.yudao.module.erp.dal.mysql.purchase.ErpPurchaseOrderItemMapper;
+import cn.iocoder.yudao.module.erp.dal.mysql.purchase.ErpPurchaseOrderMapper;
 import cn.iocoder.yudao.module.erp.enums.ErpEventEnum;
 import cn.iocoder.yudao.module.erp.enums.status.ErpPaymentStatus;
 import com.alibaba.cola.statemachine.Action;
+import com.alibaba.cola.statemachine.StateMachine;
+
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import static cn.iocoder.yudao.module.erp.enums.ErpStateMachines.PURCHASE_ORDER_PAYMENT_STATE_MACHINE_NAME;
 
 @Component
 @Slf4j
 public class ActionOrderItemPayImpl implements Action<ErpPaymentStatus, ErpEventEnum, ErpPurchaseOrderItemDO> {
     @Resource
     private ErpPurchaseOrderItemMapper itemMapper;
+    @Resource
+    private ErpPurchaseOrderMapper mapper;
+    @Resource(name = PURCHASE_ORDER_PAYMENT_STATE_MACHINE_NAME)
+    private StateMachine paymentStateMachine;
+
 
     @Override
     public void execute(ErpPaymentStatus f, ErpPaymentStatus t, ErpEventEnum erpEventEnum,
@@ -33,5 +44,14 @@ public class ActionOrderItemPayImpl implements Action<ErpPaymentStatus, ErpEvent
             context.getId(),
             f.getDesc(),
             t.getDesc());
+
+        // 4. 触发订单支付状态变更
+        // 根据订单项ID查询订单信息
+        ErpPurchaseOrderDO orderDO = mapper.selectById(context.getOrderId());
+        if (orderDO == null) {
+            log.error("未找到对应的采购订单,订单ID={}", context.getOrderId());
+            return;
+        }
+        paymentStateMachine.fireEvent(ErpPaymentStatus.fromCode(orderDO.getPayStatus()), erpEventEnum, orderDO);
     }
 }
