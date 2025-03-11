8daf081605712e9f4bc321e25174eb2e5f558006
东宇 <wangdongyu@somle.com>
2025-03-11 17:33:43 +0800

    fix 备注

--- yudao-module-erp/yudao-module-erp-api/src/main/java/cn/iocoder/yudao/module/erp/api/product/dto/ErpProductDTO.java
+++ yudao-module-erp/yudao-module-erp-api/src/main/java/cn/iocoder/yudao/module/erp/api/product/dto/ErpProductDTO.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
  */
 @Data
 @Builder
-@TableName("erp_product")
+@TableName("erp_product") //连表查询海关规则ErpCustomRuleMapper.java中需要对应上erp的产品表
 public class ErpProductDTO {
     /**
      * 产品编号
