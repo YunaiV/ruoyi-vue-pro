3756830b9c672f2a035b6aa0a29f2a8b1fbb6cac
puhui999 <puhui999@163.com>
2025-03-14 17:40:19 +0800

    【代码优化】IoT: 数据桥梁代码优化

--- yudao-module-iot/yudao-module-iot-biz/src/main/java/cn/iocoder/yudao/module/iot/controller/admin/rule/IotDataBridgeController.java
+++ yudao-module-iot/yudao-module-iot-biz/src/main/java/cn/iocoder/yudao/module/iot/controller/admin/rule/IotDataBridgeController.java
@@ -1,11 +1,8 @@
 package cn.iocoder.yudao.module.iot.controller.admin.rule;
 
-import cn.iocoder.yudao.framework.apilog.core.annotation.ApiAccessLog;
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
-import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.common.util.object.BeanUtils;
-import cn.iocoder.yudao.framework.excel.core.util.ExcelUtils;
 import cn.iocoder.yudao.module.iot.controller.admin.rule.vo.databridge.IotDataBridgePageReqVO;
 import cn.iocoder.yudao.module.iot.controller.admin.rule.vo.databridge.IotDataBridgeRespVO;
 import cn.iocoder.yudao.module.iot.controller.admin.rule.vo.databridge.IotDataBridgeSaveReqVO;
@@ -15,16 +12,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.List;
-
-import static cn.iocoder.yudao.framework.apilog.core.enums.OperateTypeEnum.EXPORT;
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
 @Tag(name = "管理后台 - IoT 数据桥梁")
@@ -75,20 +67,6 @@ public class IotDataBridgeController {
     public CommonResult<PageResult<IotDataBridgeRespVO>> getDataBridgePage(@Valid IotDataBridgePageReqVO pageReqVO) {
         PageResult<IotDataBridgeDO> pageResult = dataBridgeService.getDataBridgePage(pageReqVO);
         return success(BeanUtils.toBean(pageResult, IotDataBridgeRespVO.class));
-    }
-
-    // TODO @puhui999：不用导出哈。相关的 IotDataBridgeRespVO 里的导出也注释掉哈
-    @GetMapping("/export-excel")
-    @Operation(summary = "导出数据桥梁 Excel")
-    @PreAuthorize("@ss.hasPermission('iot:data-bridge:export')")
-    @ApiAccessLog(operateType = EXPORT)
-    public void exportDataBridgeExcel(@Valid IotDataBridgePageReqVO pageReqVO,
-                                      HttpServletResponse response) throws IOException {
-        pageReqVO.setPageSize(PageParam.PAGE_SIZE_NONE);
-        List<IotDataBridgeDO> list = dataBridgeService.getDataBridgePage(pageReqVO).getList();
-        // 导出 Excel
-        ExcelUtils.write(response, "IoT 数据桥梁.xls", "数据", IotDataBridgeRespVO.class,
-                BeanUtils.toBean(list, IotDataBridgeRespVO.class));
     }
 
 }
--- yudao-module-iot/yudao-module-iot-biz/src/main/java/cn/iocoder/yudao/module/iot/controller/admin/rule/vo/databridge/IotDataBridgeRespVO.java
+++ yudao-module-iot/yudao-module-iot-biz/src/main/java/cn/iocoder/yudao/module/iot/controller/admin/rule/vo/databridge/IotDataBridgeRespVO.java
@@ -1,18 +1,11 @@
 package cn.iocoder.yudao.module.iot.controller.admin.rule.vo.databridge;
 
-import cn.iocoder.yudao.framework.excel.core.annotations.DictFormat;
-import cn.iocoder.yudao.framework.excel.core.convert.DictConvert;
 import cn.iocoder.yudao.module.iot.controller.admin.rule.vo.databridge.config.IotDataBridgeAbstractConfig;
 import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
-import com.alibaba.excel.annotation.ExcelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-
-import static cn.iocoder.yudao.module.iot.enums.DictTypeConstants.IOT_DATA_BRIDGE_DIRECTION_ENUM;
-import static cn.iocoder.yudao.module.iot.enums.DictTypeConstants.IOT_DATA_BRIDGE_TYPE_ENUM;
-import static cn.iocoder.yudao.module.system.enums.DictTypeConstants.COMMON_STATUS;
 
 @Schema(description = "管理后台 - IoT 数据桥梁 Response VO")
 @Data
@@ -20,38 +13,27 @@ import static cn.iocoder.yudao.module.system.enums.DictTypeConstants.COMMON_STAT
 public class IotDataBridgeRespVO {
 
     @Schema(description = "桥梁编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "18564")
-    @ExcelProperty("桥梁编号")
     private Long id;
 
     @Schema(description = "桥梁名称", requiredMode = Schema.RequiredMode.REQUIRED, example = "赵六")
-    @ExcelProperty("桥梁名称")
     private String name;
 
     @Schema(description = "桥梁描述", example = "随便")
-    @ExcelProperty("桥梁描述")
     private String description;
 
     @Schema(description = "桥梁状态", requiredMode = Schema.RequiredMode.REQUIRED, example = "2")
-    @ExcelProperty(value = "桥梁状态", converter = DictConvert.class)
-    @DictFormat(COMMON_STATUS)
     private Integer status;
 
     @Schema(description = "桥梁方向", requiredMode = Schema.RequiredMode.REQUIRED)
-    @ExcelProperty(value = "桥梁方向", converter = DictConvert.class)
-    @DictFormat(IOT_DATA_BRIDGE_DIRECTION_ENUM)
     private Integer direction;
 
     @Schema(description = "桥梁类型", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
-    @ExcelProperty(value = "桥梁类型", converter = DictConvert.class)
-    @DictFormat(IOT_DATA_BRIDGE_TYPE_ENUM)
     private Integer type;
 
     @Schema(description = "桥梁配置")
-    @ExcelProperty("桥梁配置")
     private IotDataBridgeAbstractConfig config;
 
     @Schema(description = "创建时间", requiredMode = Schema.RequiredMode.REQUIRED)
-    @ExcelProperty("创建时间")
     private LocalDateTime createTime;
 
 }
