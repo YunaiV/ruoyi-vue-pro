3286c2752bf81a359160f82d5a914dcb201d90ee
gumaomao <474317126@qq.com>
2025-04-02 15:56:19 +0800

    fix:修复oms模块单元测试功能

--- yudao-module-oms/yudao-module-oms-biz/src/test/java/cn/iocoder/yudao/module/oms/service/OmsServiceTest.java
+++ yudao-module-oms/yudao-module-oms-biz/src/test/java/cn/iocoder/yudao/module/oms/service/OmsServiceTest.java
@@ -1,23 +1,26 @@
 package cn.iocoder.yudao.module.oms.service;
 
-import cn.iocoder.yudao.framework.test.core.ut.SomleBaseDbUnitTest;
+import cn.iocoder.yudao.framework.test.core.ut.BaseDbUnitTest;
 import cn.iocoder.yudao.framework.test.core.util.RandomUtils;
 import cn.iocoder.yudao.module.oms.api.dto.OmsShopSaveReqDTO;
+import cn.iocoder.yudao.module.oms.dal.mysql.OmsShopMapper;
 import cn.iocoder.yudao.module.oms.service.impl.OmsShopServiceImpl;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
-@Disabled
+//@Disabled
 @Slf4j
 @Import(OmsShopServiceImpl.class)
-public class OmsServiceTest extends SomleBaseDbUnitTest {
+public class OmsServiceTest extends BaseDbUnitTest {
     @Resource
-    OmsShopService omsShopService;
+    private OmsShopServiceImpl omsShopService;
+
+    @Resource
+    private OmsShopMapper omsShopMapper;
 
 
     @Test
