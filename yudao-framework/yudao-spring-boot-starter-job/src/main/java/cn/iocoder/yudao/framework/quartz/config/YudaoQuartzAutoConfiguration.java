eea94413e8233c7d759f4e7ee240bf85997070a0
东宇 <wangdongyu@somle.com>
2025-04-17 16:38:03 +0800

    fix 框架修改

--- yudao-framework/yudao-spring-boot-starter-job/src/main/java/cn/iocoder/yudao/framework/quartz/config/YudaoQuartzAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-job/src/main/java/cn/iocoder/yudao/framework/quartz/config/YudaoQuartzAutoConfiguration.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.Scheduler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Optional;
 
@@ -12,7 +13,7 @@ import java.util.Optional;
  * 定时任务 Configuration
  */
 @AutoConfiguration
-//@EnableScheduling // 开启 Spring 自带的定时任务
+@EnableScheduling // 开启 Spring 自带的定时任务
 @Slf4j
 public class YudaoQuartzAutoConfiguration {
 
