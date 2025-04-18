6255f027663349ab394398d3971f77559189738b
gumaomao <474317126@qq.com>
2025-04-18 16:57:30 +0800

    fix：查询易仓入库单明细上传到数仓增加过滤仓库条件

--- somle-esb/src/main/java/com/somle/esb/job/EccangReceivingDetailListDataJob.java
+++ somle-esb/src/main/java/com/somle/esb/job/EccangReceivingDetailListDataJob.java
@@ -4,7 +4,6 @@ import com.somle.eccang.model.req.EccangReceivingDetailReqVO;
 import com.somle.esb.model.OssData;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,36 +17,36 @@ public class EccangReceivingDetailListDataJob extends EccangDataJob {
     public String execute(String param) throws Exception {
         setDate(param);
 
-        List<Integer> warehouseIds = new ArrayList<>();
-        //JND-DLD 加拿大多伦多仓
-        warehouseIds.add(29);
-        //MG-CA 美国CA仓
-        warehouseIds.add(30);
-        //MG-GA-N 美国GA-N仓
-        warehouseIds.add(32);
-        //SM-DGC 索迈德国仓
-        warehouseIds.add(33);
-
-        var vo = EccangReceivingDetailReqVO.builder()
-            .dateFor(beforeYesterdayFirstSecond)
-            .dateTo(beforeYesterdayLastSecond)
-            .warehouseIds(warehouseIds)
-            .page(1)
-            .pageSize(100)
-            .build();
-        eccangService.streamReceivingDetail(vo).forEach(page -> {
-                OssData data = OssData.builder()
-                    .database(DATABASE)
-                    .tableName("receiving_detail")
-                    .syncType("inc")
-                    .requestTimestamp(System.currentTimeMillis())
-                    .folderDate(beforeYesterday)
-                    .content(page)
-                    .headers(null)
-                    .build();
-                service.send(data);
+        eccangService.getWarehouseList().forEach(eccangWarehouse -> {
+            //过滤掉仓库编码前缀是AMAZON或者Amazon的第三方仓库
+            String[] warehouseCodeArr = eccangWarehouse.getWarehouseCode().split("_");
+            String warehouseCodePrefix = warehouseCodeArr[0];
+            if (warehouseCodePrefix.equals("AMAZON") || warehouseCodePrefix.equals("Amazon")) {
+                return;
             }
-        );
+
+            var vo = EccangReceivingDetailReqVO.builder()
+                .dateFor(beforeYesterdayFirstSecond)
+                .dateTo(beforeYesterdayLastSecond)
+                .warehouseIds(List.of(eccangWarehouse.getWarehouseId()))
+                .page(1)
+                .pageSize(100)
+                .build();
+
+            eccangService.streamReceivingDetail(vo).forEach(page -> {
+                    OssData data = OssData.builder()
+                        .database(DATABASE)
+                        .tableName("receiving_detail")
+                        .syncType("inc")
+                        .requestTimestamp(System.currentTimeMillis())
+                        .folderDate(beforeYesterday)
+                        .content(page)
+                        .headers(null)
+                        .build();
+                    service.send(data);
+                }
+            );
+        });
         return "data upload success";
     }
 
