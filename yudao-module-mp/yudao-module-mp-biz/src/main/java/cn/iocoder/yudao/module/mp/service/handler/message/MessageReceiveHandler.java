5a87b33df2fd9fda3999b1558966557f2183fa05
YunaiV <zhijiantianya@gmail.com>
2025-05-04 21:26:40 +0800

    fix：【MP 公众号】receiveMessage 记录消息时，兜底 MpUserDO 的创建边界

--- yudao-module-mp/yudao-module-mp-biz/src/main/java/cn/iocoder/yudao/module/mp/service/handler/message/MessageReceiveHandler.java
+++ yudao-module-mp/yudao-module-mp-biz/src/main/java/cn/iocoder/yudao/module/mp/service/handler/message/MessageReceiveHandler.java
@@ -29,7 +29,7 @@ public class MessageReceiveHandler implements WxMpMessageHandler {
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context,
                                     WxMpService wxMpService, WxSessionManager sessionManager) {
         log.info("[handle][接收到请求消息，内容：{}]", wxMessage);
-        mpMessageService.receiveMessage(MpContextHolder.getAppId(), wxMessage);
+        mpMessageService.receiveMessage(wxMpService, MpContextHolder.getAppId(), wxMessage);
         return null;
     }
 
