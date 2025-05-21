bb380ef5175bbda4b0205d5b24159e010e0aaaee
DevDengChao <2325690622@qq.com>
2025-05-21 10:16:31 +0800

    refactor: 替换测试用例中的 WebSocket 客户端

--- yudao-server/src/test/java/cn/iocoder/yudao/framework/websocket/core/security/LoginUserHandshakeInterceptorTest.java
+++ yudao-server/src/test/java/cn/iocoder/yudao/framework/websocket/core/security/LoginUserHandshakeInterceptorTest.java
@@ -4,31 +4,33 @@ package cn.iocoder.yudao.framework.websocket.core.security;
 import cn.iocoder.yudao.framework.common.enums.UserTypeEnum;
 import cn.iocoder.yudao.server.YudaoServerApplication;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
-import okio.ByteString;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.json.JSONObject;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.handler.LoggingWebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
-import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
-@Disabled("请确保应用能正常启动以后再执行这个测试用例")
-@SpringBootTest(classes = YudaoServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@Disabled("请确保应用能正常启动以后再执行这个测试用例")
+@SpringBootTest(classes = YudaoServerApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"yudao.tenant.enable=false" /* 禁用租户功能，简化测试用例 */})
 @AutoConfigureMockMvc
 public class LoginUserHandshakeInterceptorTest {
 
@@ -53,51 +55,12 @@ public class LoginUserHandshakeInterceptorTest {
         JSONObject res = new JSONObject(content);
         String accessToken = res.getJSONObject("data").getString("accessToken");
 
-        AtomicBoolean connected = new AtomicBoolean(false);
-        AtomicBoolean disconnected = new AtomicBoolean(false);
-        OkHttpClient client = new OkHttpClient();
-        HttpUrl url = HttpUrl.get("http://localhost:" + port + "/infra/ws?token=" + accessToken);
-        Request request = new Request(url, "GET", new Headers.Builder().build(), null, new HashMap<>());
-        WebSocket webSocket = client.newWebSocket(request, new WebSocketListener() {
-            @Override
-            public void onClosed(@NotNull WebSocket webSocket, int code, @NotNull String reason) {
-                super.onClosed(webSocket, code, reason);
-                disconnected.set(true);
-                log.info("onClosed, {}, {}", code, reason);
-            }
-
-            @Override
-            public void onClosing(@NotNull WebSocket webSocket, int code, @NotNull String reason) {
-                super.onClosing(webSocket, code, reason);
-                log.info("onClosing, {}, {}", code, reason);
-            }
-
-            @Override
-            public void onFailure(@NotNull WebSocket webSocket, @NotNull Throwable t, @Nullable Response response) {
-                super.onFailure(webSocket, t, response);
-                log.info("onFailure, {}, {}", t, response);
-            }
-
-            @Override
-            public void onMessage(@NotNull WebSocket webSocket, @NotNull String text) {
-                super.onMessage(webSocket, text);
-                log.info("onMessage, {}", text);
-            }
-
-            @Override
-            public void onMessage(@NotNull WebSocket webSocket, @NotNull ByteString bytes) {
-                super.onMessage(webSocket, bytes);
-            }
-
-            @Override
-            public void onOpen(@NotNull WebSocket webSocket, @NotNull Response response) {
-                super.onOpen(webSocket, response);
-                log.info("onOpen, {}", response);
-                connected.set(true);
-            }
-        });
-        await().atMost(Duration.ofSeconds(10)).untilTrue(connected);
-        webSocket.close(1000, "close");
-        await().atMost(Duration.ofSeconds(10)).untilTrue(disconnected);
+        StandardWebSocketClient webSocketClient = new StandardWebSocketClient();
+        LoggingWebSocketHandlerDecorator webSocketHandler = new LoggingWebSocketHandlerDecorator(new TextWebSocketHandler());
+        String uri = "ws://localhost:" + port + "/infra/ws?token=" + accessToken;
+        CompletableFuture<WebSocketSession> future = webSocketClient.execute(webSocketHandler, uri);
+        WebSocketSession session = future.get(10, TimeUnit.SECONDS);
+        assertTrue(session.isOpen());
+        session.close();
     }
 }
