3f9c3661a8f3e09d6fa1aa839a87e7d1235d6712
YunaiV <zhijiantianya@gmail.com>
2025-07-15 22:39:20 +0800

    【同步】jdk21 和 jdk8 的代码

--- yudao-module-infra/src/main/java/cn/iocoder/yudao/module/infra/controller/app/file/vo/AppFileUploadReqVO.java
+++ yudao-module-infra/src/main/java/cn/iocoder/yudao/module/infra/controller/app/file/vo/AppFileUploadReqVO.java
@@ -3,10 +3,11 @@ package cn.iocoder.yudao.module.infra.controller.app.file.vo;
 import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
 
 @Schema(description = "用户 App - 上传文件 Request VO")
 @Data
