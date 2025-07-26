c25d1b37193f6985af7583723cb3c0c9e240e869
YunaiV <zhijiantianya@gmail.com>
2025-07-26 17:15:57 +0800

    review：@InDict 的实现，以及相关的使用案例

--- yudao-framework/yudao-spring-boot-starter-excel/src/main/java/cn/iocoder/yudao/framework/dict/validation/InDictCollectionValidator.java
+++ yudao-framework/yudao-spring-boot-starter-excel/src/main/java/cn/iocoder/yudao/framework/dict/validation/InDictCollectionValidator.java
@@ -1,6 +1,6 @@
 package cn.iocoder.yudao.framework.dict.validation;
 
-import cn.iocoder.yudao.framework.common.util.string.StrUtils;
+import cn.hutool.core.collection.CollUtil;
 import cn.iocoder.yudao.framework.dict.core.DictFrameworkUtils;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -19,19 +19,22 @@ public class InDictCollectionValidator implements ConstraintValidator<InDict, Co
 
     @Override
     public boolean isValid(Collection<?> list, ConstraintValidatorContext context) {
-        if (list == null) {
+        // 为空时，默认不校验，即认为通过
+        if (CollUtil.isEmpty(list)) {
             return true;
         }
+        // 校验全部通过
         List<String> dbValues = DictFrameworkUtils.getDictDataValueList(dictType);
         boolean match = list.stream().allMatch(v -> dbValues.stream()
                 .anyMatch(dbValue -> dbValue.equalsIgnoreCase(v.toString())));
         if (match) {
             return true;
         }
+
         // 校验不通过，自定义提示语句
         context.disableDefaultConstraintViolation(); // 禁用默认的 message 的值
         context.buildConstraintViolationWithTemplate(
-                context.getDefaultConstraintMessageTemplate().replaceAll("\\{value}",dbValues.toString())
+                context.getDefaultConstraintMessageTemplate().replaceAll("\\{value}", dbValues.toString())
         ).addConstraintViolation(); // 重新添加错误提示语句
         return false;
     }
--- yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/user/vo/user/UserUpdateStatusReqVO.java
+++ yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/user/vo/user/UserUpdateStatusReqVO.java
@@ -2,10 +2,11 @@ package cn.iocoder.yudao.module.system.controller.admin.user.vo.user;
 
 import cn.iocoder.yudao.framework.common.enums.CommonStatusEnum;
 import cn.iocoder.yudao.framework.common.validation.InEnum;
+import cn.iocoder.yudao.framework.dict.validation.InDict;
+import cn.iocoder.yudao.module.system.enums.DictTypeConstants;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
-
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Schema(description = "管理后台 - 用户更新状态 Request VO")
 @Data
@@ -18,6 +19,7 @@ public class UserUpdateStatusReqVO {
     @Schema(description = "状态，见 CommonStatusEnum 枚举", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     @NotNull(message = "状态不能为空")
     @InEnum(value = CommonStatusEnum.class, message = "修改状态必须是 {value}")
+    @InDict(type = DictTypeConstants.COMMON_STATUS)
     private Integer status;
 
 }
