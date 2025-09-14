8622ec9e76f26f9ea573c669fe1a3ba55ae915ae
张世彬 <happyshibin@gmail.com>
2025-09-14 16:55:23 +0900

    Delete sql directory
    delete

--- sql/tools/oracle/2_create_schema.sh
+++
@@ -1,1 +0,0 @@
-sqlplus -s ROOT/123456@//localhost/XEPDB1 @/tmp/schema.sql
--- sql/tools/sqlserver/create_schema.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -Q "CREATE DATABASE [ruoyi-vue-pro];
-GO"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -d 'ruoyi-vue-pro' -i /tmp/schema.sql
