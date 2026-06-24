ea4fa68579a915fda88bfeb78ec3a07ddd452fb5
YunaiV <zhijiantianya@gmail.com>
2026-06-24 10:59:52 -0700

    feat(pay): 新增支付渠道列表接口
    - 新增 GET /pay/channel/list 按 appId 返回渠道列表
    - Service/Mapper 补充按应用查询全部支付渠道
    - 列表转换时清空 config，避免密钥和证书配置下发到列表页
    - 保持 /pay/channel/get 返回完整配置用于编辑详情

--- yudao-module-pay/src/main/java/cn/iocoder/yudao/module/pay/convert/channel/PayChannelConvert.java
+++ yudao-module-pay/src/main/java/cn/iocoder/yudao/module/pay/convert/channel/PayChannelConvert.java
@@ -1,6 +1,7 @@
 package cn.iocoder.yudao.module.pay.convert.channel;
 
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.framework.common.util.collection.CollectionUtils;
 import cn.iocoder.yudao.module.pay.controller.admin.channel.vo.PayChannelCreateReqVO;
 import cn.iocoder.yudao.module.pay.controller.admin.channel.vo.PayChannelRespVO;
 import cn.iocoder.yudao.module.pay.controller.admin.channel.vo.PayChannelUpdateReqVO;
@@ -8,6 +9,8 @@ import cn.iocoder.yudao.module.pay.dal.dataobject.channel.PayChannelDO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface PayChannelConvert {
@@ -22,6 +25,15 @@ public interface PayChannelConvert {
 
     @Mapping(target = "config",expression = "java(cn.iocoder.yudao.framework.common.util.json.JsonUtils.toJsonString(bean.getConfig()))")
     PayChannelRespVO convert(PayChannelDO bean);
+
+    default List<PayChannelRespVO> convertList(List<PayChannelDO> list) {
+        // 列表不下发 config（密钥/证书），详情接口再返回完整配置
+        return CollectionUtils.convertList(list, channel -> {
+            PayChannelRespVO vo = convert(channel);
+            vo.setConfig(null);
+            return vo;
+        });
+    }
 
     PageResult<PayChannelRespVO> convertPage(PageResult<PayChannelDO> page);
 
