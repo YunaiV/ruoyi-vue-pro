b17432dfa2765fea609b716c1e6cb1861dc596d6
YunaiV <zhijiantianya@gmail.com>
2026-06-26 17:57:04 -0700

    fix：优化 Jackson3 NumberSerializer 继承实现
    - 继承 Jackson3 提供的 NumberSerializer
    - 复用默认数值序列化逻辑
    - 保留超出 JS 安全整数范围转字符串的处理

--- yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/json/databind/NumberSerializer.java
+++ yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/util/json/databind/NumberSerializer.java
@@ -4,10 +4,6 @@ import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonGenerator;
 import tools.jackson.databind.SerializationContext;
 import tools.jackson.databind.annotation.JacksonStdImpl;
-import tools.jackson.databind.ser.std.StdScalarSerializer;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
 
 /**
  * Long 序列化规则
@@ -17,41 +13,24 @@ import java.math.BigInteger;
  * @author 星语
  */
 @JacksonStdImpl
-public class NumberSerializer extends StdScalarSerializer<Number> {
+public class NumberSerializer extends tools.jackson.databind.ser.jdk.NumberSerializer {
 
     private static final long MAX_SAFE_INTEGER = 9007199254740991L;
     private static final long MIN_SAFE_INTEGER = -9007199254740991L;
 
     public static final NumberSerializer INSTANCE = new NumberSerializer(Number.class);
 
-    @SuppressWarnings("unchecked")
     public NumberSerializer(Class<? extends Number> rawType) {
-        super((Class<Number>) rawType);
+        super(rawType);
     }
 
     @Override
     public void serialize(Number value, JsonGenerator gen, SerializationContext serializers) throws JacksonException {
         // 超出范围 序列化位字符串
         if (value.longValue() > MIN_SAFE_INTEGER && value.longValue() < MAX_SAFE_INTEGER) {
-            writeNumber(value, gen);
+            super.serialize(value, gen, serializers);
         } else {
             gen.writeString(value.toString());
-        }
-    }
-
-    private static void writeNumber(Number value, JsonGenerator gen) throws JacksonException {
-        if (value instanceof BigDecimal decimal) {
-            gen.writeNumber(decimal);
-        } else if (value instanceof BigInteger integer) {
-            gen.writeNumber(integer);
-        } else if (value instanceof Double doubleValue) {
-            gen.writeNumber(doubleValue);
-        } else if (value instanceof Float floatValue) {
-            gen.writeNumber(floatValue);
-        } else if (value instanceof Long longValue) {
-            gen.writeNumber(longValue);
-        } else {
-            gen.writeNumber(value.intValue());
         }
     }
 }
