de70ff9529d9566ad86bf6984ba0ae64c8ac196b
YunaiV <zhijiantianya@gmail.com>
2026-07-31 09:44:49 +0800

    fix: 升级 Fastjson2 并排除 Fastjson1

--- yudao-module-pay/src/test/java/cn/iocoder/yudao/module/pay/service/channel/PayChannelServiceTest.java
+++ yudao-module-pay/src/test/java/cn/iocoder/yudao/module/pay/service/channel/PayChannelServiceTest.java
@@ -12,7 +12,6 @@ import cn.iocoder.yudao.module.pay.framework.pay.core.client.PayClient;
 import cn.iocoder.yudao.module.pay.framework.pay.core.client.PayClientFactory;
 import cn.iocoder.yudao.module.pay.framework.pay.core.client.impl.alipay.AlipayPayClientConfig;
 import cn.iocoder.yudao.module.pay.framework.pay.core.client.impl.weixin.WxPayClientConfig;
-import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -109,7 +108,7 @@ public class PayChannelServiceTest extends BaseDbUnitTest {
         AlipayPayClientConfig payClientPublicKeyConfig = randomAlipayPayClientConfig();
         PayChannelUpdateReqVO reqVO = randomPojo(PayChannelUpdateReqVO.class, o -> {
             o.setStatus(CommonStatusEnum.ENABLE.getStatus());
-            o.setConfig(JSON.toJSONString(payClientPublicKeyConfig));
+            o.setConfig(JsonUtils.toJsonString(payClientPublicKeyConfig));
         });
 
         // 调用, 并断言异常
